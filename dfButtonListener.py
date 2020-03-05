0f99e2d555c03b49569254f0da569bed05ecd92e
Shane <shane@smiskol.com>
2020-03-04 23:22:52 -0600

    test

--- dfButtonListener.py
+++ dfButtonListener.py
@@ -4,5 +4,5 @@ import cereal.messaging as messaging
 sm = messaging.SubMaster(['smiskolData'])
 while True:
   sm.update(0)
-  print(sm['smiskolData'].dfButtonTouched)
+  print(sm['smiskolData'].dfButtonStatus)
   time.sleep(1)
