ee7ea3a44f041cba558c82edae0d47172cd74489
Shane Smiskol <shane@smiskol.com>
2020-03-15 19:30:31 -0500

    don't restore kegman_conf since params probably don't overlap

--- common/op_params.py
+++ common/op_params.py
@@ -21,7 +21,7 @@ def read_params(params_file, default_params):
       params = json.load(f)
     return params, True
   except Exception as e:
-    print(e)
+    cloudlog.error(e)
     params = default_params
     return params, False
 
@@ -73,7 +73,6 @@ class opParams:
 
     self.params = {}
     self.params_file = "/data/op_params.json"
-    self.kegman_file = "/data/kegman.json"
     self.last_read_time = time.time()
     self.read_frequency = 5.0  # max frequency to read with self.get(...) (sec)
     self.force_update = False  # replaces values with default params if True, not just add add missing key/value pairs
@@ -104,13 +103,12 @@ class opParams:
   def format_default_params(self):
     return {key: self.default_params[key]['default'] for key in self.default_params}
 
-  def run_init(self):  # does first time initializing of default params, and/or restoring from kegman.json
+  def run_init(self):  # does first time initializing of default params
     if travis:
       self.params = self.format_default_params()
       return
     self.params = self.format_default_params()  # in case any file is corrupted
     to_write = False
-    no_params = False
     if os.path.isfile(self.params_file):
       self.params, read_status = read_params(self.params_file, self.format_default_params())
       if read_status:
@@ -118,18 +116,10 @@ class opParams:
         if self.delete_old():  # or if old params have been deleted
           to_write = True
       else:  # don't overwrite corrupted params, just print to screen
-        print("ERROR: Can't read op_params.json file")
-    elif os.path.isfile(self.kegman_file):
-      to_write = True  # write no matter what
-      try:
-        with open(self.kegman_file, "r") as f:  # restore params from kegman
-          self.params = json.load(f)
-          self.add_default_params()
-      except:
-        print("ERROR: Can't read kegman.json file")
+        cloudlog.error("ERROR: Can't read op_params.json file")
     else:
-      no_params = True  # user's first time running a fork with kegman_conf or op_params
-    if to_write or no_params:
+      to_write = True  # user's first time running a fork with op_params, write default params
+    if to_write:
       write_params(self.params, self.params_file)
 
   def delete_old(self):
