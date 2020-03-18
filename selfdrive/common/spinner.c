c0d650e3871e2cdf338db27b0950196fa6cf3e93
Shane Smiskol <shane@smiskol.com>
2020-03-18 08:00:57 -0500

    test

--- selfdrive/common/spinner.c
+++ selfdrive/common/spinner.c
@@ -84,7 +84,7 @@ int spin(int argc, char** argv) {
       fgets(spintext, SPINTEXT_LENGTH, stdin);
       spintext[strcspn(spintext, "\n")] = 0;
       printf("spintext: %s\n", spintext);
-      printf("idx: %s\n", strcspn(spintext, ","));
+      //printf("idx: %s\n", strchr(spintext, ","));
 
 
       // Check if number (update progress bar)
