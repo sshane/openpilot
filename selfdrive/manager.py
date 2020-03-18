a0b6bb0a5769ce98769a665921c7349a00594657
Shane Smiskol <shane@smiskol.com>
2020-03-18 09:08:12 -0500

    test

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -101,7 +101,8 @@ if not prebuilt:
           i = int(line[len(prefix):])
           if spinner is not None:
             # spinner.update("%d" % (scons_finished_progress * (i / TOTAL_SCONS_NODES)), 'compiling: {} of {}'.format(i, TOTAL_SCONS_NODES))
-            spinner.update("20")
+            spinner.update("%d" % (scons_finished_progress * (i / TOTAL_SCONS_NODES)))
+            # spinner.update("20")
         elif len(line):
           print(line.decode('utf8'))
       except Exception:
