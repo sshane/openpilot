11ab5796b292b594fa05620cf1637813c827575f
Shane Smiskol <shane@smiskol.com>
2020-03-18 10:22:31 -0500

    test

--- selfdrive/common/spinner.c
+++ selfdrive/common/spinner.c
@@ -181,8 +181,8 @@ int spin(int argc, char** argv) {
       // message
       nvgTextAlign(vg, NVG_ALIGN_CENTER | NVG_ALIGN_TOP);
       if (has_extra) {
-        nvgFontSize(vg, 61.0f);
-        nvgText(vg, fb_w/2, (fb_h*2/3)+24+91, spinstatus, NULL);
+        nvgFontSize(vg, 96.0f);
+        nvgText(vg, fb_w/2, (fb_h*2/3)+24+96, spinstatus, NULL);
       } else {
         nvgFontSize(vg, 96.0f);
         nvgText(vg, fb_w/2, (fb_h*2/3)+24, spintext, NULL);
