405e9cc6fe12f0f6637b3157af18be26eeefa66e
Shane Smiskol <shane@smiskol.com>
2020-03-18 08:14:18 -0500

    test

--- selfdrive/common/spinner.c
+++ selfdrive/common/spinner.c
@@ -85,7 +85,7 @@ int spin(int argc, char** argv) {
       spintext[strcspn(spintext, "\n")] = 0;
       printf("spintext: %s\n", spintext);
       int index = strcspn(spintext, ",");
-      char status[];
+      char* status;
       for (int i; i < index; i++){
         status[i] = spintext[i];
       }
