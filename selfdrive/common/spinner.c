987ca3faac8b008966185e66e72b27cf2e3d8543
Shane <shane@smiskol.com>
2020-03-22 23:44:18 -0500

    test

--- selfdrive/common/spinner.c
+++ selfdrive/common/spinner.c
@@ -182,14 +182,14 @@ int spin(int argc, char** argv) {
       nvgFontSize(vg, 96.0f);
       nvgText(vg, fb_w/2, (fb_h*2/3)+24, spintext, NULL);
     } else if (has_extra) {
-      char substring[4];
-      while (*spinstatus != '\0') {
-        strncpy(substring, spinstatus, 3);
-        nvgTextAlign(vg, NVG_ALIGN_CENTER | NVG_ALIGN_TOP);
-        nvgFontSize(vg, 59.0f);
-        nvgText(vg, fb_w/2, (fb_h*2/3)+24+96, spinstatus, NULL);
-        spinstatus += 3;
-      }
+//      char substring[4];
+//      while (*spinstatus != '\0') {
+//        strncpy(substring, spinstatus, 3);
+//        nvgTextAlign(vg, NVG_ALIGN_CENTER | NVG_ALIGN_TOP);
+//        nvgFontSize(vg, 59.0f);
+//        nvgText(vg, fb_w/2, (fb_h*2/3)+24+96, spinstatus, NULL);
+//        spinstatus += 3;
+//      }
     }
 
     nvgEndFrame(vg);
