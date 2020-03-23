4d6c1bcbe4e08d4f25294055ae14553344666048
Shane <shane@smiskol.com>
2020-03-22 23:48:48 -0500

    test

--- cereal/messaging/messaging.cc
+++ cereal/messaging/messaging.cc
@@ -58,7 +58,7 @@ SubSocket * SubSocket::create(Context * context, std::string endpoint, std::stri
   }
 }
 
-PubSocket * PubSocket::create(){
+PubSocket * PubSocket::create1(){
   PubSocket * s;
   if (std::getenv("ZMQ")){
     s = new ZMQPubSocket();
