0b48a03eab67a39868c265554f85bb9fda1a4275
Shane <shane@smiskol.com>
2020-03-26 00:37:29 -0500

    add live tuning for derivative

--- selfdrive/controls/lib/pid_real.py
+++ selfdrive/controls/lib/pid_real.py
@@ -1,6 +1,7 @@
 import numpy as np
 from common.numpy_fast import clip, interp
 from common.op_params import opParams
+from selfdrive.config import Conversions as CV
 
 def apply_deadzone(error, deadzone):
   if error > deadzone:
@@ -14,6 +15,8 @@ def apply_deadzone(error, deadzone):
 class PIDController():
   def __init__(self, k_p, k_i, k_d, k_f=1., pos_limit=None, neg_limit=None, rate=100, sat_limit=0.8, convert=None):
     self.op_params = opParams()
+    self.error_idx = -1
+    self.get_live_params()
     self._k_p = k_p  # proportional gain
     self._k_i = k_i  # integral gain
     self._k_d = k_d  # derivative gain
@@ -32,10 +35,17 @@ class PIDController():
     self.sat_limit = sat_limit
     self.convert = convert
 
-    self.last_error = 0.0
+    self.past_errors = []
     self.last_setpoint = 0.0
 
     self.reset()
+
+  def get_live_params(self):
+    self.last_error_idx = self.error_idx
+    self.enable_derivative = self.op_params.get('enable_derivative', True)
+    self.error_idx = self.op_params.get('error_idx', -1)
+    self.derivative = self.op_params.get('derivative', 0.16)
+    self.max_accel_d = self.op_params.get('max_accel_d', 1.0) * CV.MPH_TO_MS
 
   @property
   def k_p(self):
@@ -47,7 +57,7 @@ class PIDController():
 
   @property
   def k_d(self):
-    return interp(self.speed, self._k_d[0], self._k_d[1])
+    return self.derivative
 
   def _check_saturation(self, control, check_saturation, error):
     saturated = (control < self.neg_limit) or (control > self.pos_limit)
@@ -70,7 +80,16 @@ class PIDController():
     self.saturated = False
     self.control = 0
 
+  def set_d(self, error):
+    if len(self.past_errors) >= -self.error_idx and self.enable_derivative:
+      last_error = self.past_errors[self.error_idx]
+      rate = -self.last_error_idx / 100
+      self.d = self.k_d * ((error - last_error) / rate)
+    else:
+      self.d = 0.0
+
   def update(self, setpoint, measurement, speed=0.0, check_saturation=True, override=False, feedforward=0., deadzone=0., freeze_integrator=False):
+    self.get_live_params()
     self.speed = speed
 
     error = float(apply_deadzone(setpoint - measurement, deadzone))
@@ -82,7 +101,7 @@ class PIDController():
       self.d = 0.0
     else:
       i = self.i + error * self.k_i * self.rate
-      self.d = self.k_d * ((error - self.last_error) / self.rate)
+      self.set_d(error)
       control = self.p + self.f + i  # don't add d here
 
       if self.convert is not None:
@@ -98,14 +117,16 @@ class PIDController():
     control = self.p + self.f + self.i
     with open('/data/accel_pid', 'a') as f:
       f.write('{}\n'.format(abs(setpoint - self.last_setpoint) / self.rate))
-    if abs(setpoint - self.last_setpoint) / self.rate < 0.22352:  # if cruising with minimal setpoint change
+    if abs(setpoint - self.last_setpoint) / self.rate < self.max_accel_d:  # if cruising with minimal setpoint change
       control += self.d  # then use derivative
     if self.convert is not None:
       control = self.convert(control, speed=self.speed)
 
     self.saturated = self._check_saturation(control, check_saturation, error)
 
-    self.last_error = error
+    self.past_errors.append(error)
+    while len(self.past_errors) > 200:  # keep last 2 seconds
+      del self.past_errors[0]
     self.last_setpoint = setpoint
 
     self.control = clip(control, self.neg_limit, self.pos_limit)
