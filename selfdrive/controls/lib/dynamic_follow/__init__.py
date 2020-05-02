5495d69e34737ea2d4acca318bbaae54142e4c99
Shane <shane@smiskol.com>
2020-05-02 17:37:51 -0500

    fix recursion

--- selfdrive/controls/lib/dynamic_follow/__init__.py
+++ selfdrive/controls/lib/dynamic_follow/__init__.py
@@ -8,33 +8,7 @@ from common.travis_checker import travis
 from selfdrive.config import Conversions as CV
 from selfdrive.controls.lib.dynamic_follow.auto_df.best_so_far import predict
 from selfdrive.controls.df_alert_manager import dfAlertManager
-
-
-class LeadData:
-  v_lead = None
-  x_lead = None
-  a_lead = None
-  status = False
-  new_lead = False
-
-
-class CarData:
-  v_ego = 0.0
-  a_ego = 0.0
-
-
-class dfData:
-  v_leads = []
-  v_egos = []
-
-
-class dfProfiles:
-  traffic = 0
-  relaxed = 1
-  roadtrip = 2
-  auto = 3
-  to_profile = {0: 'traffic', 1: 'relaxed', 2: 'roadtrip', 3: 'auto'}
-  to_idx = {v: k for k, v in to_profile.items()}
+from selfdrive.controls.lib.dynamic_follow.support import LeadData, CarData, dfData, dfProfiles
 
 
 class DynamicFollow:
