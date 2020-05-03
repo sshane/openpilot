8c41800454c4f249dd6d6f8de10d3948a87a5d0a
Shane <shane@smiskol.com>
2020-05-02 19:43:41 -0500

    benchmark

--- selfdrive/controls/lib/dynamic_follow/auto_df/benchmark.py
+++ selfdrive/controls/lib/dynamic_follow/auto_df/benchmark.py
@@ -2,7 +2,7 @@ from selfdrive.controls.lib.dynamic_follow.auto_df.best_so_far import predict
 import time
 import numpy as np
 
-samples = np.random.rand(26531, 800).astype(np.float32)
+samples = np.random.rand(500, 800).astype(np.float32)
 
 t = time.time()
 for sample in samples:
