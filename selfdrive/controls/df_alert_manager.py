c50f435427a1c949e054857abb886d9d2bf909d0
Shane Smiskol <shane@smiskol.com>
2020-05-02 20:36:27 -0500

    Fix

--- selfdrive/controls/df_alert_manager.py
+++ selfdrive/controls/df_alert_manager.py
@@ -10,7 +10,7 @@ class dfAlertManager:
     self.sm = messaging.SubMaster(['dynamicFollowButton', 'dynamicFollowData'])
     self.current_profile = self.df_profiles.to_idx[self.op_params.get('dynamic_follow', default='relaxed').strip().lower()]
     self.prediction_profile = 0
-    self.auto_time = 0
+    self.auto_time = sec_since_boot()
     self.last_auto = False
 
     self.offset = None
@@ -31,14 +31,15 @@ class dfAlertManager:
       status = self.sm['dynamicFollowButton'].status
       new_profile = (status + self.offset) % len(self.df_profiles.to_profile)
       if self.last_button_status != status:
+        self.auto_time = sec_since_boot()
         changed = True
         self.op_params.put('dynamic_follow', new_profile)  # save current profile for next drive
         self.current_profile = new_profile
         self.last_button_status = status
       elif self.is_auto:
-        if not self.last_auto:
-          self.auto_time = sec_since_boot()
-          self.last_auto = True
+        #if not self.last_auto:
+        #  self.auto_time = sec_since_boot()
+        #  self.last_auto = True
         profile_pred = self.sm['dynamicFollowData'].profilePred
         changed = self.prediction_profile != profile_pred
         self.prediction_profile = profile_pred
