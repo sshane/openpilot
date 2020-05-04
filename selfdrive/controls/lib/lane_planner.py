7eed3ab78e9024f3eba6c6b447528aa815f72cde
SelfdriveVanagon <41362285+Lukilink@users.noreply.github.com>
2020-05-04 12:46:31 +0200

    Update lane_planner.py

--- selfdrive/controls/lib/lane_planner.py
+++ selfdrive/controls/lib/lane_planner.py
@@ -53,9 +53,9 @@ class LanePlanner():
     self.p_poly = [0., 0., 0., 0.]
     self.d_poly = [0., 0., 0., 0.]
 
-    self.lane_width_estimate = 4.5
+    self.lane_width_estimate = 3.7
     self.lane_width_certainty = 1.0
-    self.lane_width = 4.5
+    self.lane_width = 3.7
 
     self.l_prob = 0.
     self.r_prob = 0.
