ff3e253d1f74f8560531d791b824fb55c810becc
Shane <shane@smiskol.com>
2020-05-10 21:44:30 -0500

    color some messages

--- op_edit.py
+++ op_edit.py
@@ -11,7 +11,8 @@ class STYLES:
   OKGREEN = '\033[92m'
   WARNING = '\033[93m'
   FAIL = '\033[91m'
-  ENDC = '\033[0m'
+  CWHITE = '\33[37m'
+  ENDC = '\033[0m' + CWHITE
   BOLD = '\033[1m'
   UNDERLINE = '\033[4m'
 
