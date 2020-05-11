1591691fe40cad8f4ce4e199f85f06b395fe6464
Shane <shane@smiskol.com>
2020-05-10 21:48:14 -0500

    color some messages

--- op_edit.py
+++ op_edit.py
@@ -9,7 +9,7 @@ class STYLES:
   HEADER = '\033[95m'
   OKBLUE = '\033[94m'
   OKGREEN = '\033[92m'
-  WARNING = '\033[93m'
+  WARNING = '\033[43m'
   FAIL = '\033[91m'
   CWHITE = '\33[37m'
   ENDC = '\033[0m' + CWHITE
