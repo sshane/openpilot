52ae2c15e2daa7a60928cf6d3c29c3c32f05f1dc
Shane <shane@smiskol.com>
2020-05-15 18:57:40 -0500

    it works! now test with processes

--- helper_scripts/test_locks.py
+++ helper_scripts/test_locks.py
@@ -1,5 +1,6 @@
 from threading import Thread
 import time
+from multiprocessing import Process, Queue
 from common.op_params import opParams
 import numpy as np
 
@@ -20,11 +21,15 @@ def thread_test_read(op_p):
       print('error reading!')
 
 
+# queue = Queue()
 
 for i in range(20):
-  Thread(target=thread_test_write, args=(op_params,)).start()
+  # Thread(target=thread_test_write, args=(op_params,)).start()
+  p = Process(target=thread_test_write, args=(op_params,))
+  p.start()
 
 for i in range(80):
-  Thread(target=thread_test_read, args=(op_params,)).start()
-
+  # Thread(target=thread_test_read, args=(op_params,)).start()
+  p = Process(target=thread_test_read, args=(op_params,))
+  p.start()
 print('started all threads...')
