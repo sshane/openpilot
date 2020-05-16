f735419a4600f93d97a1e7b34b5c3387fc80f601
Shane <shane@smiskol.com>
2020-05-15 23:49:22 -0500

    add debugging message

--- selfdrive/controls/lib/dynamic_follow/__init__.py
+++ selfdrive/controls/lib/dynamic_follow/__init__.py
@@ -234,10 +234,13 @@ class DynamicFollow:
       y_dist = [1.3781, 1.3791, 1.3802, 1.3825, 1.3984, 1.4249, 1.4194, 1.3162, 1.1916, 1.0145, 0.9855, 0.9562]
       profile_mod_pos = [1.05, 1.375, 2.99, 3.8]
       profile_mod_neg = [0.79, .1, 0.0, 0.0]
-    else:  # default to relaxed/stock
+    elif df_profile == self.df_profiles.relaxed:  # default to relaxed/stock
       y_dist = [1.385, 1.394, 1.406, 1.421, 1.444, 1.474, 1.516, 1.534, 1.546, 1.568, 1.579, 1.593, 1.614]
       profile_mod_pos = [1.0] * 4
       profile_mod_neg = [1.0] * 4
+    else:
+      print('DF PROFILE: {}'.format(df_profile))
+      raise Exception('Profile not valid!')
 
     sng_TR = 1.8  # reacceleration stop and go TR
     sng_speed = 15.0 * CV.MPH_TO_MS
