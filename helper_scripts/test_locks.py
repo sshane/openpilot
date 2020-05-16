3fe93f07949a3af77dd2978fec5acde534e05dc5
Shane <shane@smiskol.com>
2020-05-15 19:08:58 -0500

    debug

--- helper_scripts/test_locks.py
+++ helper_scripts/test_locks.py
@@ -8,13 +8,13 @@ op_params = opParams()
 
 
 def thread_test_write(op_p):
-  for _ in range(1000):
+  for _ in range(100):
     # time.sleep(0.01)
     op_p.put('camera_offset', np.random.randint(0, 100))
 
 
 def thread_test_read(op_p):
-  for _ in range(1000):
+  for _ in range(100):
     # time.sleep(0.01)
     p = op_p.get('camera_offset')
     if p is None:
@@ -23,13 +23,11 @@ def thread_test_read(op_p):
 
 # queue = Queue()
 
-for i in range(20):
-  # Thread(target=thread_test_write, args=(op_params,)).start()
-  p = Process(target=thread_test_write, args=(op_params,))
-  p.start()
+for i in range(2):
+  Thread(target=thread_test_write, args=(op_params,)).start()
+  # Process(target=thread_test_write, args=(op_params,)).start()
 
-for i in range(80):
-  # Thread(target=thread_test_read, args=(op_params,)).start()
-  p = Process(target=thread_test_read, args=(op_params,))
-  p.start()
+for i in range(8):
+  Thread(target=thread_test_read, args=(op_params,)).start()
+  # Process(target=thread_test_read, args=(op_params,)).start()
 print('started all threads...')
