d9ec8a4791fc87d0c2e97107f07de9514248b913
Shane <shane@smiskol.com>
2020-05-17 00:10:48 -0500

    debug import and init time

--- op_edit.py
+++ op_edit.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-from common.op_params import opParams
 import time
+t = time.time()
+from common.op_params import opParams
+print(time.time() - t)
 import ast
 import difflib
 
@@ -28,7 +30,9 @@ class STYLES:
 
 class opEdit:  # use by running `python /data/openpilot/op_edit.py`
   def __init__(self):
+    t = time.time()
     self.op_params = opParams()
+    print(time.time() - t)
     self.params = None
     self.sleep_time = 1.0
     self.live_tuning = self.op_params.get('op_edit_live_mode', False)
