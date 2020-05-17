6bbc4d143be5b035beb453789ccb70824bdc0406
Shane <shane@smiskol.com>
2020-05-17 00:18:19 -0500

    get rid of cloudlog

--- common/op_params.py
+++ common/op_params.py
@@ -209,7 +209,7 @@ class opParams:
         self.params = json.load(f)
       return True
     except Exception as e:
-      print(e)
+      print('op_params: {}'.format(e)))
       self.params = self._format_default_params()
       return False
 
