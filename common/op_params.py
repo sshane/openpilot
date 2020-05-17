45c8f7b2317fbbff4b495761057d6e7b07ad3ea2
Shane <shane@smiskol.com>
2020-05-17 00:38:49 -0500

    debug time

--- common/op_params.py
+++ common/op_params.py
@@ -213,8 +213,8 @@ class opParams:
     if not travis:
       with open(self.params_file, "w") as f:
         # json.dump(self.params, f, indent=2, sort_keys=True)
-        # f.write(json.dumps(self.params, indent=2, sort_keys=True))
-        f.write(str(self.params))
+        f.write(json.dumps(self.params, indent=2, sort_keys=True))
+        # f.write(str(self.params))
 
 
 op_params = opParams()
