d197e3dac6b77b5f00957dcd380e53bc5a16ed12
Shane <shane@smiskol.com>
2020-05-17 01:40:19 -0500

    debug

--- op_edit.py
+++ op_edit.py
@@ -78,7 +78,10 @@ class opEdit:  # use by running `python /data/openpilot/op_edit.py`
 
       to_print = []
       for idx, param in enumerate(self.params):
-        line = '{}{}. {}: {}  {}'.format(STYLES.OKGREEN if idx == self.last_choice and self.last_choice is not None else '', idx + 1, param, values_list[idx], live[idx])
+        line = '{}. {}: {}  {}'.format(idx + 1, param, values_list[idx], live[idx])
+        if idx == self.last_choice and self.last_choice is not None:
+          line = STYLES.OKGREEN + line
+        # line = '{}{}. {}: {}  {}'.format(STYLES.OKGREEN if idx == self.last_choice and self.last_choice is not None else '', idx + 1, param, values_list[idx], live[idx])
         to_print.append(line)
 
       # to_print = ['{}{}. {}: {}  {}'.format(STYLES.OKGREEN if idx == self.last_choice and self.last_choice is not None else '', idx + 1, i, values_list[idx], live[idx]) for idx, i in enumerate(self.params)]
