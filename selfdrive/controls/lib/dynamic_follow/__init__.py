d48ebaed5272bd34b9daff452d46807e24331a2d
Shane Smiskol <shane@smiskol.com>
2020-05-19 11:47:13 -0500

    Just in case

--- selfdrive/controls/lib/dynamic_follow/__init__.py
+++ selfdrive/controls/lib/dynamic_follow/__init__.py
@@ -282,8 +282,8 @@ class DynamicFollow:
 
     # rel_accel_mod = self._calculate_relative_accel_new()
     # if rel_accel_mod is not None:  # if available
-      # TR_mods.append(rel_accel_mod)
-      # pass
+    #  # TR_mods.append(rel_accel_mod)
+    #  # pass
 
     # Profile modifications - Designed so that each profile reacts similarly to changing lead dynamics
     profile_mod_pos = interp(self.car_data.v_ego, profile_mod_x, profile_mod_pos)
