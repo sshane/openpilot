9e4dd6da1582369151ba5005e7331eb7e3e50fc3
Shane Smiskol <shane@smiskol.com>
2020-05-19 02:12:45 -0500

    Fix for now

--- selfdrive/controls/lib/dynamic_follow/__init__.py
+++ selfdrive/controls/lib/dynamic_follow/__init__.py
@@ -105,7 +105,7 @@ class DynamicFollow:
     if self.mpc_id == 1 and self.pm is not None:
       dat = messaging.new_message()
       dat.init('dynamicFollowData')
-      dat.dynamicFollowData.mpcTR = self.TR
+      dat.dynamicFollowData.mpcTR = 1.8  # self.TR
       dat.dynamicFollowData.profilePred = self.model_profile
       self.pm.send('dynamicFollowData', dat)
 
