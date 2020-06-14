0227ee2959a7e1a2fe6cc151750454dcfd5b00bb
Shane <shane@smiskol.com>
2020-06-14 01:47:42 -0500

    revert

--- op_edit.py
+++ op_edit.py
@@ -9,7 +9,7 @@ class STYLES:
   # HEADER = '\033[95m'
   # OKBLUE = '\033[94m'
   # CBLUE = '\33[44m'
-  # BOLD = '\033[1m'#
+  # BOLD = '\033[1m'
   OKGREEN = '\033[92m'
   CWHITE = '\33[37m'
   ENDC = '\033[0m' + CWHITE
