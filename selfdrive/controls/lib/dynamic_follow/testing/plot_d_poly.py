cf1b3b4995c0f1deb59be6649375232956c076bb
Shane <shane@smiskol.com>
2020-06-19 01:29:05 -0500

    wow, that was crazy accurate. just an update

--- selfdrive/controls/lib/dynamic_follow/testing/plot_d_poly.py
+++ selfdrive/controls/lib/dynamic_follow/testing/plot_d_poly.py
@@ -8,10 +8,13 @@ import time
 
 os.chdir(os.getcwd())
 
-data = 'lane_speed'
+data = 'lane_speed2'
+
+# good ones: 134000, 107823 + 900, 98708, 98708+9000, 117734
+start = 117734
 
 with open(data, 'r') as f:
-  data = f.read().split('\n')[:-1][10400:12000]
+  data = f.read().split('\n')[:-1][start:start+30000]
 
 data_parsed = []
 for idx, line in enumerate(data):
@@ -21,11 +24,16 @@ for idx, line in enumerate(data):
   line = ast.literal_eval(line)
   if len(line['d_poly']) == 0:
     continue
+  # if abs(line['v_ego'] * 2.2369 - 57) < 0.2235 and len(line['live_tracks']) > 1:
+  if len(line['live_tracks']) > 2 and np.mean([trk['vRel'] for trk in line['live_tracks'] if trk['vRel'] > -2]) > -5 and line['v_ego'] > 5:
+    print(line['v_ego'] * 2.2369)
+    print(idx)
+    print()
   data_parsed.append(line)
 data = data_parsed
 
 # dPoly = [line['d_poly'] for line in data]
-max_dist = 91
+max_dist = 200
 preprocessed = []
 
 for idx, line in enumerate(data):
@@ -39,6 +47,8 @@ for idx, line in enumerate(data):
   preprocessed[-1]['x'] = x
   preprocessed[-1]['y'] = y
 
+  preprocessed[-1]['live_tracks'] = line['live_tracks']
+
   # for track in line['live_tracks']:
   #   plt.plot(track['dRel'], track['yRel'], 'bo')
 
@@ -47,22 +57,27 @@ for idx, line in enumerate(data):
   # plt.xlabel('longitudinal position (m)')
   # plt.ylabel('lateral position (m)')
   # plt.xlim(0, max_dist)
-  ylim = [min(min(y), -20), max(max(y), 20)]
+  ylim = [min(min(y), -7.5), max(max(y), 7.5)]
   preprocessed[-1]['ylim'] = ylim
   # plt.ylim(*ylim)
   # plt.pause(0.01)
 
-preprocessed = preprocessed[::3]
-
+preprocessed = preprocessed[::12]
+plt.clf()
+plt.pause(0.01)
+input('press any key to continue')
 for line in preprocessed:
   t = time.time()
   plt.clf()
   plt.title(line['title'])
 
-  # for track in line['live_tracks']:
-  #   plt.plot(track['dRel'], track['yRel'], 'bo')
+  for track in line['live_tracks']:
+    if track['vRel'] > -5:
+      plt.plot(track['dRel'], track['yRel'], 'bo')
 
   plt.plot(line['x'], line['y'], label='desired path')
+  plt.plot(line['x'], line['y'] + 3.7 / 2, 'r--', label='lane line')
+  plt.plot(line['x'], line['y'] - 3.7 / 2, 'r--', label='lane line')
   plt.show()
 
   plt.legend()
