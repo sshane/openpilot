2be3d4cb2db5841f5b84d6056715fd4dca6618f2
Shane <shane@smiskol.com>
2020-06-23 22:33:30 -0500

    debug

--- selfdrive/controls/lib/dynamic_follow/testing/pm_test.py
+++ selfdrive/controls/lib/dynamic_follow/testing/pm_test.py
@@ -10,12 +10,15 @@ msg.laneSpeed.rightAvgSpeed = 54.4
 msg.laneSpeed.status = 'right'
 msg.laneSpeed.new = False
 pm.send('laneSpeed', msg)
+
 input('press enter')
 
 msg = messaging.new_message('laneSpeed')
 msg.laneSpeed.status = 'right'
 msg.laneSpeed.new = False
 pm.send('laneSpeed', msg)
+
+input('press enter')
 
 msg = messaging.new_message('laneSpeed')
 msg.laneSpeed.leftAvgSpeed = 42.4
