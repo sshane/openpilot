36fbe492f6027755e1f70d50121689bf5ead72b8
Shane Smiskol <shane@smiskol.com>
2020-06-28 19:27:14 -0500

    less cost mod

--- selfdrive/controls/lib/dynamic_follow/__init__.py
+++ selfdrive/controls/lib/dynamic_follow/__init__.py
@@ -137,7 +137,7 @@ class DynamicFollow:
 
     change_time = sec_since_boot() - self.profile_change_time
     change_time_x = [0, 1.5, 4]  # for three seconds after effective profile has changed
-    change_mod_y = [1, 15, 1]  # multiply cost by multiplier to quickly change distance
+    change_mod_y = [1, 7.5, 1]  # multiply cost by multiplier to quickly change distance
     if change_time < change_time_x[-1]:  # if profile changed in last 3 seconds
       cost *= interp(change_time, change_time_x, change_mod_y)
 
