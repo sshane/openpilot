333e17b1b16b06044528ced526328f6d65b3055c
Shane Smiskol <shane@smiskol.com>
2020-06-30 00:57:39 -0500

    final updates, remove old files

--- selfdrive/controls/lib/dynamic_follow/testing/test_ls_button.py
+++
@@ -1,8 +0,0 @@
-from cereal.messaging import SubMaster
-
-sm = SubMaster(['laneSpeedButton'])
-
-while 1:
-  sm.update(0)
-  print('status: {}'.format(sm['laneSpeedButton'].status))
-  input('press enter')
