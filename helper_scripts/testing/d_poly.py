398947830172e04ece66ad0f3316e72e105ef8a0
Shane Smiskol <shane@smiskol.com>
2020-07-11 14:11:43 -0500

    tuning

--- helper_scripts/testing/d_poly.py
+++ helper_scripts/testing/d_poly.py
@@ -1,6 +1,6 @@
 import numpy as np
-l_prob = 0.25
-r_prob = 0.25
+l_prob = 0.5
+r_prob = .9
 path_from_left_lane = np.array([0.25, 0.25, 0.25, 0.25])
 path_from_right_lane = np.array([0.75, 0.75, 0.75, 0.75])
 lr_prob = l_prob + r_prob - l_prob * r_prob
@@ -8,5 +8,5 @@ d_poly_lane = (l_prob * path_from_left_lane + r_prob * path_from_right_lane) / (
 print(lr_prob)
 print(d_poly_lane)  # should be [0.5, 0.5, 0.5, 0.5]
 
-p_poly = np.array([1, 1, 1, 1])
+p_poly = np.array([10, -15, 25, 0])
 print(lr_prob * d_poly_lane + (1.0 - lr_prob) * p_poly)
