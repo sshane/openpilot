f79e1ca74345b409cfebb76fed3174ba548a5ad0
Shane Smiskol <shane@smiskol.com>
2020-07-13 16:49:22 -0500

    write datetime when reboot is triggered

--- selfdrive/updated.py
+++ selfdrive/updated.py
@@ -322,6 +322,8 @@ def auto_update_reboot(time_offroad, need_reboot, new_version):
 
   if sec_since_boot() - time_offroad > min_reboot_time and need_reboot:  # allow reboot x minutes after stopping openpilot or starting EON
     cloudlog.info(COLORS.RED + "AUTO UPDATE: REBOOTING" + COLORS.ENDC)
+    with open('/data/reboots', 'a') as f:
+      f.write('{}: Auto update triggered reboot\n'.format(datetime.datetime.now()))
     run(["am", "start", "-a", "android.intent.action.REBOOT"])
   elif need_reboot:
     cloudlog.info(COLORS.BLUE_GREEN + "UPDATE FOUND, waiting {} sec. until reboot".format(min_reboot_time - (sec_since_boot() - time_offroad)) + COLORS.ENDC)
