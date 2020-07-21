1266edba4e1b3b7e1d528d0481a61c19a5de07ab
Shane Smiskol <shane@smiskol.com>
2020-07-20 20:13:04 -0500

    reverse the if check so that we do speed check first

--- selfdrive/controls/lib/lane_speed.py
+++ selfdrive/controls/lib/lane_speed.py
@@ -177,35 +177,23 @@ class LaneSpeed:
 
     for track, y_offset in zip(self.live_tracks, y_offsets):
       travk_vel = track.vRel + self.v_ego
-      if self.lanes['left'].bounds[0] + y_offset >= track.yRel >= self.lanes['left'].bounds[1] + y_offset:  # track is in a lane
-        if travk_vel >= 2.24:
+      if travk_vel >= 2.24:
+        if self.lanes['left'].bounds[0] + y_offset >= track.yRel >= self.lanes['left'].bounds[1] + y_offset:
           self.lanes['left'].tracks.append(track)
-        elif travk_vel <= -2.24:  # make sure we don't add stopped tracks at high speeds
-          self.lanes['left'].oncoming_tracks.append(track)
-      elif self.lanes['right'].bounds[0] + y_offset >= track.yRel >= self.lanes['right'].bounds[1] + y_offset:  # track is in a lane
-        if travk_vel >= 2.24:
-          self.lanes['right'].tracks.append(track)
-        elif travk_vel <= -2.24:  # make sure we don't add stopped tracks at high speeds
-          self.lanes['right'].oncoming_tracks.append(track)
-      elif self.lanes['middle'].bounds[0] + y_offset >= track.yRel >= self.lanes['middle'].bounds[1] + y_offset:  # track is in a lane
-        if travk_vel >= 2.24:
+        elif self.lanes['middle'].bounds[0] + y_offset >= track.yRel >= self.lanes['middle'].bounds[1] + y_offset:
           self.lanes['middle'].tracks.append(track)
-        elif travk_vel <= -2.24:  # make sure we don't add stopped tracks at high speeds
+        elif self.lanes['right'].bounds[0] + y_offset >= track.yRel >= self.lanes['right'].bounds[1] + y_offset:
+          self.lanes['right'].tracks.append(track)
+      elif travk_vel <= -2.24:  # make sure we don't add stopped tracks at high speeds
+        if self.lanes['left'].bounds[0] + y_offset >= track.yRel >= self.lanes['left'].bounds[1] + y_offset:
+          self.lanes['left'].oncoming_tracks.append(track)
+        elif self.lanes['middle'].bounds[0] + y_offset >= track.yRel >= self.lanes['middle'].bounds[1] + y_offset:
           self.lanes['middle'].oncoming_tracks.append(track)
-
-
-      # for lane_name in self.lanes:
-        # lane_bounds = [b + y_offset for b in self.lanes[lane_name].bounds]  # offset lane bounds based on our future lateral position (dPoly) and track's distance
-        # if lane_bounds[0] >= track.yRel >= lane_bounds[1]:  # track is in a lane
-        #   travk_vel = track.vRel + self.v_ego
-        #   if travk_vel >= 2.24:
-        #     self.lanes[lane_name].tracks.append(track)
-        #   elif travk_vel <= -2.24:  # make sure we don't add stopped tracks at high speeds
-        #     self.lanes[lane_name].oncoming_tracks.append(track)
-        #   break  # skip to next track
+        elif self.lanes['right'].bounds[0] + y_offset >= track.yRel >= self.lanes['right'].bounds[1] + y_offset:
+          self.lanes['right'].oncoming_tracks.append(track)
 
     # t_iter = 0
-    # for track, y_offset in zip(self.live_tracks, y_offsets):
+    # for track, y_offset in zip(self.live_tracks, y_offsets):  # 1299.8735 hz
     #   for lane_name in self.lanes:
     #     t_iter += 1
     #     lane_bounds = [b + y_offset for b in self.lanes[lane_name].bounds]  # offset lane bounds based on our future lateral position (dPoly) and track's distance
