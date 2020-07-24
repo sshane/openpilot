52ac6fb4f4dbb8a1cdbf97d47c241ccd3df9a624
Shane Smiskol <shane@smiskol.com>
2020-07-23 22:53:43 -0500

    change coords

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -667,8 +667,8 @@ static void ui_draw_ml_button(UIState *s) {
   int btn_w = 400;
   int btn_h = 150;
   int y_padding = 50;
-  int btn_x = 1051 - btn_w / 2;  // 150 + 50 padding
-  int btn_y = 934 - btn_h / 2 - y_padding;
+  int btn_x = 960 - btn_w / 2;  // 150 + 50 padding
+  int btn_y = 1080 - btn_h / 2 - y_padding;
 
 //  ui_draw_image(s->vg, btn_x, btn_y, btn_w, btn_h, test_button, 1.0f);
 
