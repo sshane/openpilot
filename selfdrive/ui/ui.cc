c304dd4c29b3789a2012f89dff98a69ccc90de8d
Shane Smiskol <shane@smiskol.com>
2020-07-24 02:53:31 -0500

    debug

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -148,7 +148,6 @@ static bool handle_ml_touch(UIState *s, int touch_x, int touch_y) {
 //    int btn_h = 138;
 //    if (s->awake && s->vision_connected && s->status != STATUS_STOPPED) {
     if (true) {
-        int padding = 40;
         int xs[2] = {1920 / 2 - btn_w / 2, 1920 / 2 + btn_w / 2};
         int y_top = 915;
         if (xs[0] <= touch_x && xs[1] >= touch_x && y_top <= touch_y) {
