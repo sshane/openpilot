389bcc958eb2a8364846bcc7d9a01b37f422b66c
Shane Smiskol <shane@smiskol.com>
2020-07-23 21:41:31 -0500

    test

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -811,8 +811,6 @@ int main(int argc, char* argv[]) {
 
   bool debug_ui = false;
   printf("%d\n", argc);
-  printf("%s\n", argv[0]);
-  printf("%s\n", argv[1]);
   if (argc == 2) {
     if (strcmp(argv[1], "debug") == 0) {
       debug_ui = true;
