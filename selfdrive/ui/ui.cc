7dfa26e01604ef256c6ff029f0580a02aabd78e3
Shane Smiskol <shane@smiskol.com>
2020-07-26 01:07:43 -0500

    test json

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -305,7 +305,8 @@ static void ui_init_vision(UIState *s, const VisionStreamBufs back_bufs,
   std::string "{\"test\": \"hello\"}";
   auto json = json11::Json::parse(str, err);
   std::string new_fingerprint = json["test"].string_value();
-  printf("value: %s\n", new_fingerprint)
+//  printf("value: %s\n", new_fingerprint);
+  cout << new_fingerprint << endl;
 
   Json::Reader reader;
   Json::Value op_params;
