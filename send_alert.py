91172d2604115dd596cfa9a0c073978d8963368d
Shane Smiskol <shane@smiskol.com>
2020-07-29 03:34:46 -0500

    debug alerts

---
+++ send_alert.py
@@ -0,0 +1,11 @@
+from selfdrive.controls.lib.alertmanager import AlertManager
+from cereal import car, log
+
+EventName = car.CarEvent.EventName
+
+AM = AlertManager()
+frame = 0
+while True:
+  input()
+  AM.add_custom(frame, EventName.laneSpeedKeeping, False, extra_text_1='RIGHT', extra_text_2='Oncoming traffic in left lane')
+  frame += 500
