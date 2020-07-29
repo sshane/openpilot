98d24ec63d240796dd5f016c9197bc6316624709
Shane Smiskol <shane@smiskol.com>
2020-07-29 03:36:03 -0500

    debug alerts

--- send_alert.py
+++ send_alert.py
@@ -8,4 +8,5 @@ frame = 0
 while True:
   input()
   AM.add_custom(frame, EventName.laneSpeedKeeping, False, extra_text_1='RIGHT', extra_text_2='Oncoming traffic in left lane')
+  AM.process_alerts(frame)
   frame += 500
