e3bba7189c00013c5b66a899522bfcc9847b3793
Shane Smiskol <shane@smiskol.com>
2020-07-31 17:56:42 -0500

    test writing to file

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -7,4 +7,4 @@ DATE=$(date +%d-%m-%y--%I:%M%p)
 LOG_DIR="/data/community/logs/$DATE"
 
 export PASSIVE="0"
-exec ./launch_chffrplus.sh >&1 | tee "$LOG_DIR"
+exec ./launch_chffrplus.sh |& tee -a "$LOG_DIR"
