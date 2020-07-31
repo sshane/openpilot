96ba3c44f67ab07df6701397778f2572d4c14c7f
Shane Smiskol <shane@smiskol.com>
2020-07-31 17:52:23 -0500

    test writing to file

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -3,8 +3,8 @@
 [ ! -d "/data/community" ] && mkdir /data/community
 [ ! -d "/data/community/logs" ] && mkdir /data/community/logs && echo "Created /data/community/logs"
 
-$ DATE="date +%d-%m-%y--%I:%M%p"
-$ LOG_DIR="/data/community/logs/$DATE"
+DATE="date +%d-%m-%y--%I:%M%p"
+LOG_DIR="/data/community/logs/$DATE"
 
 export PASSIVE="0"
 exec ./launch_chffrplus.sh &> "$LOG_DIR"
