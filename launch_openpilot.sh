258d268fd28b691961a56d31af3ea66f0f93a8b5
Shane Smiskol <shane@smiskol.com>
2020-07-31 17:53:58 -0500

    test writing to file

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -3,7 +3,7 @@
 [ ! -d "/data/community" ] && mkdir /data/community
 [ ! -d "/data/community/logs" ] && mkdir /data/community/logs && echo "Created /data/community/logs"
 
-DATE="date +%d-%m-%y--%I:%M%p"
+DATE=$(date +%d-%m-%y--%I:%M%p)
 LOG_DIR="/data/community/logs/$DATE"
 
 export PASSIVE="0"
