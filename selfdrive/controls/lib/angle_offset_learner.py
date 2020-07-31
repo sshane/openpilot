59f39b1d9cc7206d3f0fc16d3433f7c21556c41e
Shane Smiskol <shane@smiskol.com>
2020-07-30 20:24:00 -0500

    change to offset

--- selfdrive/controls/lib/angle_offset_learner.py
+++ selfdrive/controls/lib/angle_offset_learner.py
@@ -11,7 +11,7 @@ from selfdrive.config import Conversions as CV
 
 class AngleOffsetLearner:
   def __init__(self):
-    self.curvature_file = '/data/curvature_offset_v5.json'
+    self.offset_file = '/data/angle_offset_v5.json'
     rate = 1 / 20.  # pathplanner is 20 hz
     self.learning_rate = 3.5e-3 * rate
     self.write_frequency = 5  # in seconds
@@ -19,7 +19,7 @@ class AngleOffsetLearner:
     self.directions = ['left', 'right']
     self.speed_bands = ['slow', 'medium', 'fast']
     self.angle_bands = ['center', 'inner', 'outer']
-    self._load_curvature()
+    self._load_offsets()
 
   def update(self, angle_steers, d_poly, v_ego):
     offset = 0
@@ -32,7 +32,7 @@ class AngleOffsetLearner:
       offset = self.learned_offsets[direction][angle_band][speed_band]
 
     if sec_since_boot() - self._last_write_time >= self.write_frequency:
-      self._write_curvature()
+      self._write_offsets()
     return clip(offset, -3, 3)
 
   def pick_speed_band(self, v_ego):
@@ -52,18 +52,18 @@ class AngleOffsetLearner:
       return 'outer', direction  # between +=[5, inf)
     return None, direction  # return none when below +-0.1, removes possibility of returning offset in this case
 
-  def _load_curvature(self):
+  def _load_offsets(self):
     self._last_write_time = 0
     try:
-      with open(self.curvature_file, 'r') as f:
+      with open(self.offset_file, 'r') as f:
         self.learned_offsets = json.load(f)
       return
     except:  # can't read file or doesn't exist
       self.learned_offsets = {d: {s: {a: 0 for a in self.angle_bands} for s in self.speed_bands} for d in self.directions}
-      self._write_curvature()  # rewrite/create new file
+      self._write_offsets()  # rewrite/create new file
 
-  def _write_curvature(self):
-    with open(self.curvature_file, 'w') as f:
+  def _write_offsets(self):
+    with open(self.offset_file, 'w') as f:
       f.write(json.dumps(self.learned_offsets, indent=2))
-    os.chmod(self.curvature_file, 0o777)
+    os.chmod(self.offset_file, 0o777)
     self._last_write_time = sec_since_boot()
