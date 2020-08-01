254d72f38588c2d859a80fcd88499fbb5e32d6fb
Shane Smiskol <shane@smiskol.com>
2020-07-31 20:10:32 -0500

    debug

--- selfdrive/temp.py
+++ selfdrive/temp.py
@@ -6,12 +6,13 @@ from cereal.messaging import SubMaster
 sm = SubMaster(['logMessage'])
 
 last = ''
+msg = ''
 while 1:
-  try:
-    msg = ast.literal_eval(sm['logMessage'])['msg']
-  except:
-    continue
   while msg == last:
+    try:
+      msg = ast.literal_eval(sm['logMessage'])['msg']
+    except:
+      continue
     sm.update(0)
   last = str(msg)
   print(last)
