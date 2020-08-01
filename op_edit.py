daeb47b6120241206f99c5963f7687801e8c9f6e
Shane Smiskol <shane@smiskol.com>
2020-08-01 10:16:06 -0500

    yikes, that was ugly 🤮

--- op_edit.py
+++ op_edit.py
@@ -77,14 +77,7 @@ class opEdit:  # use by running `python /data/openpilot/op_edit.py`
         if idx == self.last_choice and self.last_choice is not None:
           line = COLORS.OKGREEN + line
         else:
-          v = self.params[param]
-          if len(str(v)) < 20:
-            if type(v) in self.type_colors:
-              v_color = self.type_colors[type(v)]
-              if isinstance(v, bool):
-                v_color = v_color[v]
-
-          line = v_color + line
+          line = COLORS.CYAN + line
         to_print.append(line)
 
       extras = {'a': 'Add new parameter',
