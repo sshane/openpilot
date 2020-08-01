a3ec5b7be8fd0ab8acb29074a02e4cc6ac2d1a6f
Shane Smiskol <shane@smiskol.com>
2020-07-31 20:05:29 -0500

    debug

--- selfdrive/temp.py
+++ selfdrive/temp.py
@@ -5,7 +5,7 @@ from cereal.messaging import SubMaster
 sm = SubMaster(['logMessage'])
 
 while 1:
-  sm.update(0)
+  while not sm.updated['logMessage']:
+    sm.update(0)
   print(sm['logMessage'])
-  time.sleep(1)
 # print("PRINT")
