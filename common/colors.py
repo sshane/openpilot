03ab2500130088f4f6ef4f86f83544c72cda26ff
Shane Smiskol <shane@smiskol.com>
2020-08-01 09:08:10 -0500

    test

--- common/colors.py
+++ common/colors.py
@@ -1,6 +1,7 @@
 class COLORS:
-  def BASE(col):  # seems to support more colors
+  def BASE(self, col):  # seems to support more colors
     return '\33[38;5;{}m'.format(col)
+
   HEADER = '\033[95m'
   OKBLUE = '\033[94m'
   CBLUE = '\33[44m'
@@ -10,23 +11,28 @@ class COLORS:
   ENDC = '\033[0m' + CWHITE
   UNDERLINE = '\033[4m'
   PINK = '\33[38;5;207m'
-  PRETTY_YELLOW = BASE.format(220)
+  PRETTY_YELLOW = BASE(220)
 
   RED = '\033[91m'
   PURPLE_BG = '\33[45m'
   YELLOW = '\033[93m'
-  BLUE_GREEN = BASE.format(85)
+  BLUE_GREEN = BASE(85)
 
   FAIL = RED
   INFO = PURPLE_BG
   SUCCESS = OKGREEN
   PROMPT = YELLOW
   DBLUE = '\033[36m'
-  CYAN = BASE.format(39)
+  CYAN = BASE(39)
   WARNING = '\033[33m'
+
+
+COLORS = COLORS()
+
 
 def opParams_warning(msg):
   print('{}opParams WARNING: {}{}'.format(COLORS.WARNING, msg, COLORS.ENDC))
+
 
 def opParams_error(msg):
   print('{}opParams ERROR: {}{}'.format(COLORS.FAIL, msg, COLORS.ENDC))
