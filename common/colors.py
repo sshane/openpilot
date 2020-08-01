6df88ea62f19e6202b40c50c1f94420ca298393d
Shane Smiskol <shane@smiskol.com>
2020-08-01 09:09:46 -0500

    fix

--- common/colors.py
+++ common/colors.py
@@ -1,30 +1,31 @@
 class COLORS:
+  def __init__(self):
+    self.HEADER = '\033[95m'
+    self.OKBLUE = '\033[94m'
+    self.CBLUE = '\33[44m'
+    self.BOLD = '\033[1m'
+    self.OKGREEN = '\033[92m'
+    self.CWHITE = '\33[37m'
+    self.ENDC = '\033[0m' + self.CWHITE
+    self.UNDERLINE = '\033[4m'
+    self.PINK = '\33[38;5;207m'
+    self.PRETTY_YELLOW = self.BASE(220)
+
+    self.RED = '\033[91m'
+    self.PURPLE_BG = '\33[45m'
+    self.YELLOW = '\033[93m'
+    self.BLUE_GREEN = self.BASE(85)
+
+    self.FAIL = self.RED
+    self.INFO = self.PURPLE_BG
+    self.SUCCESS = self.OKGREEN
+    self.PROMPT = self.YELLOW
+    self.DBLUE = '\033[36m'
+    self.CYAN = self.BASE(39)
+    self.WARNING = '\033[33m'
+
   def BASE(self, col):  # seems to support more colors
     return '\33[38;5;{}m'.format(col)
-
-  HEADER = '\033[95m'
-  OKBLUE = '\033[94m'
-  CBLUE = '\33[44m'
-  BOLD = '\033[1m'
-  OKGREEN = '\033[92m'
-  CWHITE = '\33[37m'
-  ENDC = '\033[0m' + CWHITE
-  UNDERLINE = '\033[4m'
-  PINK = '\33[38;5;207m'
-  PRETTY_YELLOW = BASE(220)
-
-  RED = '\033[91m'
-  PURPLE_BG = '\33[45m'
-  YELLOW = '\033[93m'
-  BLUE_GREEN = BASE(85)
-
-  FAIL = RED
-  INFO = PURPLE_BG
-  SUCCESS = OKGREEN
-  PROMPT = YELLOW
-  DBLUE = '\033[36m'
-  CYAN = BASE(39)
-  WARNING = '\033[33m'
 
 
 COLORS = COLORS()
