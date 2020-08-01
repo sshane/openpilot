b258543763e55c9e049276045de4619399c2df5c
Shane Smiskol <shane@smiskol.com>
2020-08-01 08:49:00 -0500

    remove useless args

--- op_edit.py
+++ op_edit.py
@@ -259,7 +259,7 @@ class opEdit:  # use by running `python /data/openpilot/op_edit.py`
   def success(self, msg, sleep_time=None, end=''):
     if sleep_time is None:
       sleep_time = self.sleep_time
-    msg = self.str_color(msg, style='success', surround=False, underline=False)
+    msg = self.str_color(msg, style='success')
 
     print(msg, flush=True, end='\n' + end)
     time.sleep(sleep_time)
