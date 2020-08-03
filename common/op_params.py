185b90d397dce372a3622b230129190c27b46db9
Shane Smiskol <shane@smiskol.com>
2020-08-02 22:27:44 -0500

    Fix Param() giving error

--- common/op_params.py
+++ common/op_params.py
@@ -20,7 +20,7 @@ class ValueTypes:
 
 
 class Param:
-  def __init__(self, default=None, allowed_types=None, description=None, hidden=False):
+  def __init__(self, default=None, allowed_types=[], description=None, hidden=False):
     self.default = default
     if not isinstance(allowed_types, list):
       allowed_types = [allowed_types]
