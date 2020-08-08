c74e155f70d46842a0c80f18ec95c2eade3916ab
Shane Smiskol <shane@smiskol.com>
2020-08-08 14:53:14 -0500

    debug

--- debug_df_button.py
+++ debug_df_button.py
@@ -5,4 +5,5 @@ sm = messaging.SubMaster(['dynamicFollowButton', 'dynamicFollowData'])
 while 1:
   sm.update(0)
   print('status: {}'.format(sm['dynamicFollowButton'].status))
+  print('updated: {}'.format(sm.updated['dynamicFollowButton']))
   time.sleep(1)
