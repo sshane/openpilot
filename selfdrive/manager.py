64d929f9d675743a07048170ec56f0462a3325c0
Shane Smiskol <shane@smiskol.com>
2020-08-26 23:57:49 -0500

    now reset

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -610,7 +610,7 @@ if __name__ == "__main__":
     if exit_status == 'reset':
       try:
         subprocess.check_output(["git", "pull"], cwd=BASEDIR)
-        # subprocess.check_output(["git", "reset", "--hard", "@{u}"], cwd=BASEDIR)
+        subprocess.check_output(["git", "reset", "--hard", "@{u}"], cwd=BASEDIR)
         print('GIT SUCCESS')
       except subprocess.CalledProcessError as e:
         print('GIT ERROR')
