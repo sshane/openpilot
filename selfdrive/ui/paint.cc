285e91897b4dd79aa03b1bdc35527332d8008f45
Shane Smiskol <shane@smiskol.com>
2020-08-29 22:46:26 -0500

    debug

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -323,10 +323,10 @@ static void update_all_lane_lines_data(UIState *s, const PathData &path, model_p
 }
 
 static void ui_draw_lane(UIState *s, const PathData *path, model_path_vertices_data *pstart, float prob) {
-//  ui_draw_lane_line(s, pstart, prob, path, false);
+  ui_draw_lane_line(s, pstart, prob, path, false);
 //  color.a /= 25;
   prob /= 25;
-  ui_draw_lane_line(s, pstart + 1, prob, path, false);
+//  ui_draw_lane_line(s, pstart + 1, prob, path, false);  // so is this
 //  ui_draw_lane_line(s, pstart + 2, prob, path, false); // this is how confident model is
 }
 
