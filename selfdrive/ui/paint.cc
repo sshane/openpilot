95fae4046c5bf47e1e480789e98c7698ee50da3d
Shane Smiskol <shane@smiskol.com>
2020-08-29 22:34:44 -0500

    debug

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -145,7 +145,6 @@ static void ui_draw_lane_line(UIState *s, const model_path_vertices_data *pvd, f
     nvgLineTo(s->vg, pvd->v[i].x, pvd->v[i].y);
   }
   nvgClosePath(s->vg);
-  std::cout << use_col << "\n";
   if (use_col){
     std::cout << "prob: " << prob << std::endl;
 //      nvgFillColor(s->vg, nvgHSLA(hue, 73, 64, prob * 255.));  // get redder when line is closer to car. hsla divides a by 255f
