aa27fdb3f827cad2aca0e5eef852c20ad1c84404
Shane Smiskol <shane@smiskol.com>
2020-09-04 23:38:06 -0500

    not needed

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -311,7 +311,7 @@ static void ui_draw_lane(UIState *s, const PathData *path, model_path_vertices_d
 //  float dists[2] = {1.4, 1.0};
 //  float hues[2] = {133, 0};  // green to red
   float hue = 332.5 * lane_pos - 332.5;  // equivalent to above interp code
-  hue = fmin(133., fmax(0., hue)) / 360.;  // clip and normalize
+  hue = fmin(133, fmax(0, hue)) / 360.;  // clip and normalize
   NVGcolor color = nvgHSLA(hue, 0.73, 0.64, prob * 255);
 
   ui_draw_lane_line(s, pstart, color);
