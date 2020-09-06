03e3c517689c248a9acf954d7900a0b5f08fd13a
Shane Smiskol <shane@smiskol.com>
2020-09-06 00:31:23 -0500

    revert

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -220,7 +220,7 @@ static void ui_draw_track(UIState *s, bool is_mpc, track_vertices_data *pvd, con
   float TR = 1.8;  // eval car position at 1.8s from path
   float lat_pos = p_poly[2] * TR + p_poly[1] * pow(TR, 2) + p_poly[0] * pow(TR, 3);
   lat_pos = std::abs(lat_pos);
-  float offset = lat_pos * -1973.33;
+  float offset = lat_pos * -987;
   std::cout << "offset: " << offset << std::endl;
 
 
