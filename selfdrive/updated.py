f9283c6f40aea34dd8cb38f89f9e92120da6836c
Shane Smiskol <shane@smiskol.com>
2020-09-10 15:53:02 -0500

    revert auto update changes

--- selfdrive/updated.py
+++ selfdrive/updated.py
@@ -271,7 +271,7 @@ def finalize_from_ovfs_copy():
   cloudlog.info("done finalizing overlay")
 
 
-def attempt_update(time_offroad, need_reboot):
+def attempt_update():
   cloudlog.info("attempting git update inside staging overlay")
 
   setup_git_options(OVERLAY_MERGED)
@@ -314,24 +314,24 @@ def attempt_update(time_offroad, need_reboot):
     cloudlog.info("nothing new from git at this time")
 
   set_update_available_params(new_version=new_version)
-  return auto_update_reboot(time_offroad, need_reboot, new_version)
+  # return auto_update_reboot(time_offroad, need_reboot, new_version)
 
 
-def auto_update_reboot(time_offroad, need_reboot, new_version):
-  min_reboot_time = 5. * 60
-  if new_version:
-    need_reboot = True
-
-  if need_reboot:
-    if sec_since_boot() - time_offroad > min_reboot_time:
-      cloudlog.info(COLORS.RED + "AUTO UPDATE: REBOOTING" + COLORS.ENDC)
-      with open('/data/reboot_events.txt', 'a') as f:
-        f.write('{}: Auto update triggered reboot\n'.format(datetime.datetime.now()))
-      run(["am", "start", "-a", "android.intent.action.REBOOT"])
-    else:
-      cloudlog.info(COLORS.BLUE_GREEN + "UPDATE FOUND, waiting {} sec. until reboot".format(min_reboot_time - (sec_since_boot() - time_offroad)) + COLORS.ENDC)
-
-  return need_reboot
+# def auto_update_reboot(time_offroad, need_reboot, new_version):
+#   min_reboot_time = 5. * 60
+#   if new_version:
+#     need_reboot = True
+#
+#   if need_reboot:
+#     if sec_since_boot() - time_offroad > min_reboot_time:
+#       cloudlog.info(COLORS.RED + "AUTO UPDATE: REBOOTING" + COLORS.ENDC)
+#       with open('/data/reboot_events.txt', 'a') as f:
+#         f.write('{}: Auto update triggered reboot\n'.format(datetime.datetime.now()))
+#       run(["am", "start", "-a", "android.intent.action.REBOOT"])
+#     else:
+#       cloudlog.info(COLORS.BLUE_GREEN + "UPDATE FOUND, waiting {} sec. until reboot".format(min_reboot_time - (sec_since_boot() - time_offroad)) + COLORS.ENDC)
+#
+#   return need_reboot
 
 
 def main():
@@ -361,8 +361,6 @@ def main():
   time.sleep(30)
   wait_helper = WaitTimeHelper()
 
-  time_offroad = 0
-  need_reboot = False
   while True:
     update_failed_count += 1
     time_wrong = datetime.datetime.utcnow().year < 2019
@@ -387,11 +385,8 @@ def main():
           overlay_init_done = True
 
         if params.get("IsOffroad") == b"1":
-          need_reboot = attempt_update(time_offroad, need_reboot)
+          attempt_update()
           update_failed_count = 0
-        else:
-          time_offroad = sec_since_boot()
-          cloudlog.info("not running updater, openpilot running")
 
       except subprocess.CalledProcessError as e:
         cloudlog.event(
