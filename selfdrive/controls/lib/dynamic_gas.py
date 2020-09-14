43ced17698ac7379e4509cfc024dae502632b655
Shane Smiskol <shane@smiskol.com>
2020-09-14 14:02:30 -0500

    add to DG

--- selfdrive/controls/lib/dynamic_gas.py
+++ selfdrive/controls/lib/dynamic_gas.py
@@ -76,7 +76,7 @@ class DynamicGas:
         y = [0.218, 0.222, 0.233, 0.25, 0.273, 0.294, 0.337, 0.362, 0.38, 0.389, 0.398, 0.41, 0.421, 0.459, 0.512, 0.564, 0.621]
         y = [interp(i, [0.218, (0.218 + 0.398) / 2, 0.398], [1.075 * i, i * 1.05, i]) for i in y]  # more gas at lower speeds up until ~40 mph
         self.supported_car = True
-      elif self.candidate == CAR_TOYOTA.PRIUS:
+      elif self.candidate in [CAR_TOYOTA.PRIUS, CAR_TOYOTA.PRIUS_2020]:
         x = [0.0, 1.4082, 2.8031, 4.2266, 5.3827, 6.1656, 7.2478, 8.2831, 10.2447, 12.964, 15.423, 18.119, 20.117, 24.4661, 29.0581, 32.7101, 35.7633]
         y = [0.3, 0.304, 0.315, 0.342, 0.365, 0.386, 0.429, 0.454, 0.472, 0.48, 0.489, 0.421, 0.432, 0.480, 0.55, 0.621, 0.7]
         self.supported_car = True
