379231a621bd5889c641141f2d1c76213fcd1678
Shane Smiskol <shane@smiskol.com>
2020-09-14 14:05:28 -0500

    rm

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -341,7 +341,6 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.indi.actuatorEffectiveness = 6.0
 
     ret.steerRateCost = 0.5 if ret.hasZss else 1.0
-    # ret.steerRateCost = 1.0
     ret.centerToFront = ret.wheelbase * 0.44
 
     # TODO: get actual value, for now starting with reasonable value for
