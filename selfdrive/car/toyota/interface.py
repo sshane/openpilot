e52fd2622e98e8b6169613a6a9f0f9f067e341be
Shane Smiskol <shane@smiskol.com>
2020-09-16 15:49:18 -0500

    try 2

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -97,7 +97,7 @@ class CarInterface(CarInterfaceBase):
       # ret.lateralTuning.indi.actuatorEffectiveness = 1.0
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.6], [0.1]]
       ret.lateralTuning.pid.kdBP = [0.]
-      ret.lateralTuning.pid.kdV = [1.2]  # from birdman6450
+      ret.lateralTuning.pid.kdV = [2.]  # corolla D times gain in PI values
       ret.lateralTuning.pid.kf = 0.00007818594
       ret.steerActuatorDelay = 0.55
 
