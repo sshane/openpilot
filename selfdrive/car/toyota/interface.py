e8d3af585cef29c83d0f752f8efcf1807f189132
Shane Smiskol <shane@smiskol.com>
2020-09-16 15:45:07 -0500

    try this for derivative

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -97,7 +97,7 @@ class CarInterface(CarInterfaceBase):
       # ret.lateralTuning.indi.actuatorEffectiveness = 1.0
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.6], [0.1]]
       ret.lateralTuning.pid.kdBP = [0.]
-      ret.lateralTuning.pid.kdV = [9.0]  # from birdman6450
+      ret.lateralTuning.pid.kdV = [1.2]  # from birdman6450
       ret.lateralTuning.pid.kf = 0.00007818594
       ret.steerActuatorDelay = 0.55
 
