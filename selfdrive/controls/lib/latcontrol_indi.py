68c3153a5d1959ae0c612105605baa4e82b3b5ef
SCshredder17 <57152954+SCshredder17@users.noreply.github.com>
2020-10-09 22:34:53 -0700

    Update latcontrol_indi.py
    Updated @property, deleted old line

--- selfdrive/controls/lib/latcontrol_indi.py
+++ selfdrive/controls/lib/latcontrol_indi.py
@@ -37,7 +37,6 @@ class LatControlINDI():
 
     self.RC = CP.lateralTuning.indi.timeConstant
     self.G = CP.lateralTuning.indi.actuatorEffectiveness
-    self.outer_loop_gain = CP.lateralTuning.indi.outerLoopGain
     self.inner_loop_gain = CP.lateralTuning.indi.innerLoopGain
     self.alpha = 1. - DT_CTRL / (self.RC + DT_CTRL)
 
@@ -46,7 +45,7 @@ class LatControlINDI():
 
     self.reset()
     
-    @property
+  @property
   def outer_loop_gain(self):
     return interp(self.v_ego, self.CP.lateralTuning.indi.outerLoopGainBP, self.CP.lateralTuning.indi.outerLoopGainV)
 
