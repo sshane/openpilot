fcac369e0dc61a31b3266270b836802a7c58b325
Shane Smiskol <shane@smiskol.com>
2020-10-15 15:33:18 -0500

    remove

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -88,8 +88,6 @@ from multiprocessing import Process
 spinner = Spinner()
 spinner.update("0")
 
-scons_start_t = sec_since_boot()
-
 if not prebuilt:
   for retry in [True, False]:
     # run scons
@@ -154,8 +152,6 @@ if not prebuilt:
         exit(1)
     else:
       break
-
-print('\033[91mSCONS TOOK: {}'.format(sec_since_boot() - scons_start_t))
 
 import cereal
 import cereal.messaging as messaging
