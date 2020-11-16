4df888d730016eff27391492fcd9c933345d212f
Kumar <36933347+rav4kumar@users.noreply.github.com>
2020-11-16 13:24:45 -0700

    Update ui.hpp

--- selfdrive/ui/ui.hpp
+++ selfdrive/ui/ui.hpp
@@ -107,6 +107,8 @@ typedef struct UIScene {
   std::string alert_type;
   cereal::ControlsState::AlertSize alert_size;
 
+  bool recording;
+  
   cereal::HealthData::HwType hwType;
   int satelliteCount;
   NetStatus athenaStatus;
