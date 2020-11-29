64550bdbfb73174c08f2a79e75e5833f7f77590b
birdman6450 <67883352+birdman6450@users.noreply.github.com>
2020-11-28 19:35:50 -0800

    Update op_params.py

--- common/op_params.py
+++ common/op_params.py
@@ -98,6 +98,7 @@ class opParams:
                         'use_lqr': Param(False, bool, 'Enable this to use LQR as your lateral controller over default with any car'),
                         'corollaTSS2_use_indi': Param(False, bool, 'Enable this to use INDI for lat with your TSS2 Corolla'),
                         'rav4TSS2_use_indi': Param(False, bool, 'Enable this to use INDI for lat with your TSS2 RAV4'),
+                        'pruisTSS1_use_communityindi': Param(False, bool, 'Enable this to use community INDI for lat with your TSS1 Pruis'),
                         'standstill_hack': Param(False, bool, 'Some cars support stop and go, you just need to enable this')}
 
     self._params_file = '/data/op_params.json'
