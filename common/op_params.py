e4a2834f234a243fffe091dd6a78eae76e7babae
birdman6450 <67883352+birdman6450@users.noreply.github.com>
2020-11-28 16:07:32 -0800

    Update op_params.py

--- common/op_params.py
+++ common/op_params.py
@@ -101,6 +101,7 @@ class opParams:
                         'prius_use_pid': Param(False, bool, 'This enables the PID lateral controller with new a experimental derivative tune\nFalse: stock INDI, True: TSS2-tuned PID'),
                         'use_lqr': Param(False, bool, 'Enable this to use LQR as your lateral controller over default with any car'),
                         'corollaTSS2_use_indi': Param(False, bool, 'Enable this to use INDI for lat with your Corolla with TSS2'),
+                        'rav4TSS2_use_indi': Param(False, bool, 'Enable this to use INDI for lat with RAV4 with TSS2')
                         'standstill_hack': Param(False, bool, 'Some cars support stop and go, you just need to enable this')}
 
     self._params_file = '/data/op_params.json'
