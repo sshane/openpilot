32e1322a1408f3c58790853a73f28774f0b57a1e
Shane Smiskol <shane@smiskol.com>
2021-01-11 23:56:44 -0600

    test red

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -364,7 +364,7 @@ static void ui_draw_vision_speed(UIState *s) {
 
   snprintf(speed_str, sizeof(speed_str), "%d", (int)speed);
   bool braking = true;
-  ui_draw_text(s->vg, viz_rect.centerX(), 240, speed_str, 96*2.5, braking?nvgRGBA(255, 135, 135, 255):COLOR_WHITE, s->font_sans_bold);
+  ui_draw_text(s->vg, viz_rect.centerX(), 240, speed_str, 96*2.5, braking?nvgRGBA(255, 66, 66, 255):COLOR_WHITE, s->font_sans_bold);
   ui_draw_text(s->vg, viz_rect.centerX(), 320, s->is_metric?"km/h":"mph", 36*2.5, COLOR_WHITE_ALPHA(200), s->font_sans_regular);
 }
 
