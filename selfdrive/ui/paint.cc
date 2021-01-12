d2992e66bd355065a13ab55016a42e62e8d90575
Shane Smiskol <shane@smiskol.com>
2021-01-12 00:21:53 -0600

    if brake > 1%

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -363,8 +363,8 @@ static void ui_draw_vision_speed(UIState *s) {
   nvgTextAlign(s->vg, NVG_ALIGN_CENTER | NVG_ALIGN_BASELINE);
 
   snprintf(speed_str, sizeof(speed_str), "%d", (int)speed);
-  bool braking = true;
-  std::cout << s->scene.car_control.getActuators().getGas();
+  bool braking = s->scene.car_control.getActuators().getBrake() > 0.01f;
+  std::cout << s->scene.car_control.getActuators().getBrake() << std::endl;
   ui_draw_text(s->vg, viz_rect.centerX(), 240, speed_str, 96*2.5, braking?nvgRGBA(255, 66, 66, 255):COLOR_WHITE, s->font_sans_bold);
   ui_draw_text(s->vg, viz_rect.centerX(), 320, s->is_metric?"km/h":"mph", 36*2.5, COLOR_WHITE_ALPHA(200), s->font_sans_regular);
 }
