545f07aa248a3a107eb2d10b5fd68985a09a083b
Shane Smiskol <shane@smiskol.com>
2021-01-22 03:51:52 -0600

    Add interp_2d function to numpy_fast

--- common/numpy_fast.py
+++ common/numpy_fast.py
@@ -18,5 +18,37 @@ def interp(x, xp, fp):
 
   return [get_interp(v) for v in x] if hasattr(x, '__iter__') else get_interp(x)
 
+def interp_2d(x, y, bp, v):
+  N_x = len(bp[0])
+  N_y = len(bp[1])
+
+  def get_interp(xv, yv):
+    hi_x = 0
+    hi_y = 0
+    while hi_x < N_x and xv > bp[0][hi_x]:  # get hi for x bp
+      hi_x += 1
+    while hi_y < N_y and yv > bp[1][hi_y]:  # then get hi for y bp
+      hi_y += 1
+    low_x = hi_x - 1
+    low_y = hi_y - 1
+
+    if ((hi_x == N_x or hi_y == N_y) and xv > bp[0][low_x] and yv > bp[1][low_y]) or hi_x == 0 or hi_y == 0:
+      # This branch is taken if either x or y is at top or bottom of their respective BPs
+      if hi_x == N_x and hi_y == N_y:  # both top
+        return v[-1][-1]
+      elif hi_x == 0 and hi_y == 0:  # both bottom
+        return v[0][0]
+      # Since we know either x or y is at top or bottom we figure out which is what and then just do one interpolation
+      # (These two variables use the same if check, it's not as complicated as it looks; this is just shorthand)
+      bp_idx = 1 if hi_x in [N_x, 0] else 0
+      new_v = v[-1 if hi_x == N_x else 0] if hi_x in [N_x, 0] else [_v[-1 if hi_y == N_y else 0] for _v in v]
+      return interp(yv if bp_idx == 1 else xv, bp[bp_idx], new_v)
+    else:  # This branch is taken if both x and y are in between BPs
+      # Iterates through zipped low and high x values, then interpolates with x to get a new value list we can interpolate y with
+      new_v = [interp(xv, [bp[0][low_x], bp[0][hi_x]], [low_xv, hi_xv]) for low_xv, hi_xv in zip(v[low_x], v[hi_x])]
+      return interp(yv, [bp[1][low_y], bp[1][hi_y]], new_v)
+
+  return [get_interp(v1, v2) for v1, v2 in zip(x, y)] if hasattr(x, '__iter__') else get_interp(x, y)
+
 def mean(x):
   return sum(x) / len(x)
