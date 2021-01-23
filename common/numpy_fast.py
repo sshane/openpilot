ad9a9c81a930f28ec33fc20c7b6e40574bdb2378
Shane Smiskol <shane@smiskol.com>
2021-01-23 09:47:10 -0600

    Some better interp_2d function comments and clean up

--- common/numpy_fast.py
+++ common/numpy_fast.py
@@ -33,20 +33,22 @@ def interp_2d(x, y, bp, v):
     low_y = hi_y - 1
 
     if ((hi_x == N_x or hi_y == N_y) and xv > bp[0][low_x] and yv > bp[1][low_y]) or hi_x == 0 or hi_y == 0:
-      # This branch is taken if either x or y is at top or bottom of their respective BPs
-      if hi_x == N_x and hi_y == N_y:  # both top
+      # This branch is taken if either x or y is at top or bottom of their respective BPs (just need to interpolate one input)
+      if hi_x == N_x and hi_y == N_y:  # both at top
         return v[-1][-1]
-      elif hi_x == 0 and hi_y == 0:  # both bottom
+      elif hi_x == 0 and hi_y == 0:  # both at bottom
         return v[0][0]
       # Since we know either x or y is at top or bottom we figure out which is what and then just do one interpolation
-      # (These two variables use the same if check, it's not as complicated as it looks; this is just shorthand)
-      bp_idx = 1 if hi_x in [N_x, 0] else 0
-      new_v = v[-1 if hi_x == N_x else 0] if hi_x in [N_x, 0] else [_v[-1 if hi_y == N_y else 0] for _v in v]
+      bp_idx = 1 if hi_x in [N_x, 0] else 0  # Use y BPs if x is at top or bottom, else use x BPs (y is at top or bottom)
+      # Use last or first values if x is at top or bottom else get last or first value of each x values list (y is at top or bottom)
+      new_v = v[-1 if hi_x == N_x else 0] if hi_x in [N_x, 0] else (
+               [_v[-1 if hi_y == N_y else 0] for _v in v])
       return interp(yv if bp_idx == 1 else xv, bp[bp_idx], new_v)
-    else:  # This branch is taken if both x and y are in between BPs
-      # Iterates through zipped low and high x values, then interpolates with x to get a new value list we can interpolate y with
-      new_v = [interp(xv, [bp[0][low_x], bp[0][hi_x]], [low_xv, hi_xv]) for low_xv, hi_xv in zip(v[low_x], v[hi_x])]
-      return interp(yv, [bp[1][low_y], bp[1][hi_y]], new_v)
+    else:  # This branch is taken if both x and y are not top or bottom (interpolate both)
+      # Gets us a new values list by interpolating x input first
+      new_v = [interp(xv, [bp[0][low_x], bp[0][hi_x]], [low_xv, hi_xv]) for
+               low_xv, hi_xv in zip(v[low_x], v[hi_x])]
+      return interp(yv, [bp[1][low_y], bp[1][hi_y]], new_v)  # Finally we can interpolate y input
 
   return [get_interp(v1, v2) for v1, v2 in zip(x, y)] if hasattr(x, '__iter__') else get_interp(x, y)
 
