1fb086ee8b0d0f91fc583be70ed7b78b97994ebf
Shane Smiskol <shane@smiskol.com>
2021-02-14 21:19:10 -0600

    more realistic

--- selfdrive/common/test_op_params.py
+++ selfdrive/common/test_op_params.py
@@ -1,10 +1,13 @@
 from common.op_params import opParams
 import threading
+import time
+import random
 
 threads = 10
 
 def start():
   op_params = opParams()
+  time.sleep(random.uniform(0.2, 0.7))
   op_params.get('camera_offset')
   op_params.put('camera_offset', 0.1)
   op_params.get('camera_offset')
