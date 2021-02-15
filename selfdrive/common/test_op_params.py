93324d3d6d64ef6c20e7f9e96f473dc4dedfe6ae
Shane Smiskol <shane@smiskol.com>
2021-02-14 21:30:19 -0600

    debug

--- selfdrive/common/test_op_params.py
+++ selfdrive/common/test_op_params.py
@@ -7,7 +7,7 @@ threads = 5
 
 def start():
   op_params = opParams()
-  time.sleep(random.uniform(0.00001, 0.0001))
+  # time.sleep(random.uniform(0.00001, 0.0001))
   op_params.get('camera_offset')
   op_params.put('camera_offset', 0.1)
   op_params.get('camera_offset')
