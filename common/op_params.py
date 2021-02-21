e317446c058dd2cfee9ab10ee04b29afc675fbfa
Shane Smiskol <shane@smiskol.com>
2021-02-20 20:09:09 -0600

    fix altering dict while iterating on it

--- common/op_params.py
+++ common/op_params.py
@@ -178,7 +178,7 @@ class opParams:
 
   def _delete_and_reset(self):
     needs_write = False
-    for param in self.params:
+    for param in self.params.keys():
       if param in self._to_delete:
         del self.params[param]
         needs_write = True
