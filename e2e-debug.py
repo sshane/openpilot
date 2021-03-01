731b4fb4d49722d38686da51305353c1bdec400e
Shane Smiskol <shane@smiskol.com>
2021-03-01 04:10:06 -0600

    get t

--- e2e-debug.py
+++ e2e-debug.py
@@ -2,9 +2,9 @@ import time
 import cereal.messaging as messaging
 
 sm = messaging.SubMaster(['modelV2'])
-
-# while True:
-#   sm.update(0)
-#   print(sm['modelV2'].position.t)
-
 print(sm['modelV2'].position.t)
+
+while True:
+  sm.update(0)
+  print(sm['modelV2'].position.t)
+
