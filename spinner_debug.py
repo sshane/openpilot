b1aff05c2aa8333d69df3e126291a956231a574a
Shane Smiskol <shane@smiskol.com>
2021-03-09 12:50:57 -0600

    debug

---
+++ spinner_debug.py
@@ -0,0 +1,16 @@
+import time
+from common.spinner import Spinner
+import sys
+
+spinner = Spinner()
+spinner.update_progress(0, 100)
+
+argv = sys.argv[1:]
+rate = 1 / float(argv[0])
+
+for i in range(100):
+  print(i)
+  spinner.update_progress(i, 100)
+  time.sleep(rate)
+time.sleep(5)
+spinner.close()
