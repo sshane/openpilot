d0e289f7f195e05cc0e1aa801c7d3a5fafc5c700
Shane Smiskol <shane@smiskol.com>
2021-03-10 17:30:57 -0600

    clean up

--- common/op_params.py
+++ common/op_params.py
@@ -149,6 +149,42 @@ class opParams:
     self._to_reset = []  # a list of params you want reset to their default values
     self._run_init()  # restores, reads, and updates params
 
+  def _run_init(self):  # does first time initializing of default params
+    # Two required parameters for opEdit
+    self.fork_params['username'] = Param(None, [type(None), str, bool], 'Your identifier provided with any crash logs sent to Sentry.\nHelps the developer reach out to you if anything goes wrong')
+    self.fork_params['op_edit_live_mode'] = Param(False, bool, 'This parameter controls which mode opEdit starts in', hidden=True)
+
+    self.params = self._load_params(can_import=True)
+    self._add_default_params()  # adds missing params and resets values with invalid types to self.params
+    self._delete_and_reset()  # removes old params
+
+  def get(self, key=None, force_update=False):  # key=None returns dict of all params
+    if key is None:
+      return self._get_all_params(to_update=force_update)
+
+    self._check_key_exists(key, 'get')
+    param_info = self.fork_params[key]
+
+    if not param_info.static or force_update:
+      value, success = _read_param(key)
+      if not success:  # in case of read error, use default and overwrite param
+        value = param_info.default_value
+        _write_param(key, value)
+      self.params[key] = value
+
+    if param_info.is_valid(value := self.params[key]):
+      return value  # all good, returning user's value
+
+    print(warning('User\'s value type is not valid! Returning default'))  # somehow... it should always be valid
+    return param_info.default_value  # return default value because user's value of key is not in allowed_types to avoid crashing openpilot
+
+  def put(self, key, value):
+    self._check_key_exists(key, 'put')
+    if not self.fork_params[key].is_valid(value):
+      raise Exception('opParams: Tried to put a value of invalid type!')
+    self.params.update({key: value})
+    _write_param(key, value)
+
   def _load_params(self, can_import=False):
     ret = _import_params(can_import)  # returns success (bool), params (dict)
     if ret[0]:
@@ -166,42 +202,10 @@ class opParams:
       params[key] = value
     return params
 
-  def _run_init(self):  # does first time initializing of default params
-    # Two required parameters for opEdit
-    self.fork_params['username'] = Param(None, [type(None), str, bool], 'Your identifier provided with any crash logs sent to Sentry.\nHelps the developer reach out to you if anything goes wrong')
-    self.fork_params['op_edit_live_mode'] = Param(False, bool, 'This parameter controls which mode opEdit starts in', hidden=True)
-
-    self.params = self._load_params(can_import=True)
-    print(self.params)
-    self._add_default_params()  # adds missing params and resets values with invalid types to self.params
-    self._delete_and_reset()  # removes old params
-
-  def get(self, key=None, force_update=False):  # key=None returns dict of all params
-    if key is None:
-      return self._get_all_params(to_update=force_update)
-
-    self._check_key_exists(key, 'get')
-    param_info = self.fork_params[key]
-
-    if not param_info.static or force_update:
-      value, success = _read_param(key)
-      if not success:
-        value = param_info.default_value
-        _write_param(key, value)
-      self.params[key] = value
-
-    if param_info.is_valid(value := self.params[key]):
-      return value  # all good, returning user's value
-
-    print(warning('User\'s value type is not valid! Returning default'))  # somehow... it should always be valid
-    return param_info.default_value  # return default value because user's value of key is not in allowed_types to avoid crashing openpilot
-
-  def put(self, key, value):
-    self._check_key_exists(key, 'put')
-    if not self.fork_params[key].is_valid(value):
-      raise Exception('opParams: Tried to put a value of invalid type!')
-    self.params.update({key: value})
-    _write_param(key, value)
+  def _get_all_params(self, to_update=False):
+    if to_update:
+      self.params = self._load_params()
+    return {k: self.params[k] for k, p in self.fork_params.items() if k in self.params and not p.hidden}
 
   def _check_key_exists(self, key, met):
     if key not in self.fork_params:
@@ -225,8 +229,3 @@ class opParams:
       elif key in self._to_reset and key in self.fork_params:
         self.params[key] = self.fork_params[key].default_value
         _write_param(key, self.params[key])
-
-  def _get_all_params(self, to_update=False):
-    if to_update:
-      self.params = self._load_params()
-    return {k: self.params[k] for k, p in self.fork_params.items() if k in self.params and not p.hidden}
