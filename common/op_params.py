0893e26ba2251bbb962385fc63a271959e14bbd6
Shane Smiskol <shane@smiskol.com>
2021-03-11 22:03:05 -0600

    New function, last gave too much gas

--- common/op_params.py
+++ common/op_params.py
@@ -108,7 +108,7 @@ class opParams:
                                                      '4: Average of last two functions (too fast, too slow)\n'
                                                      '5: Less gas under 5 mph and low accel, more gas above 5 mph and high accel\n'
                                                      '6: 5 but less gas all around\n'
-                                                     '7: 4 but less gas at low accel', live=True),
+                                                     '7: 4 but more coefficients', live=True),
                         'coast_function': Param(0, int, '0: original function, 1: new function with less accel offset at low speed. should be more accurate', live=True),
 
                         'prius_use_pid': Param(False, bool, 'This enables the PID lateral controller with new a experimental derivative tune\n'
