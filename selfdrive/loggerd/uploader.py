f0e7d994859acdffedbeac899e4978cbff86f759
Shane Smiskol <shane@smiskol.com>
2021-03-15 18:17:21 -0500

    Revert

--- selfdrive/loggerd/uploader.py
+++ selfdrive/loggerd/uploader.py
@@ -226,7 +226,6 @@ def uploader_fn(exit_event):
 
     d = None
     on_hotspot = is_on_hotspot()
-    opParams().get('unknown_param')
     if (on_hotspot and upload_on_hotspot) or not on_hotspot:
       d = uploader.next_file_to_upload(with_raw=allow_raw_upload and on_wifi and offroad)
 
