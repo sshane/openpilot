b9a828c307a1157aec2bb3df303cd76a1e3cf705
Shane Smiskol <shane@smiskol.com>
2021-03-26 20:55:11 -0500

    Test

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -35,24 +35,28 @@ void sa_init(UIState *s, bool full_init) {
 
   s->ui_debug = false;  // change to true while debugging
 
-  // stock additions todo: run opparams first (in main()?) to ensure json values exist
-  std::ifstream op_params_file("/data/op_params.json");
-  std::string op_params_content((std::istreambuf_iterator<char>(op_params_file)),
-                                (std::istreambuf_iterator<char>()));
+  char** value;
+  size_t sz;
+  *value = static_cast<char*>(read_file("/data/op_params.json".c_str(), value_sz));
 
-  std::string err;
-  auto json = json11::Json::parse(op_params_content, err);
-  if (!json.is_null() && err.empty()) {
-    printf("successfully parsed opParams json\n");
-    s->scene.dfButtonStatus = DF_TO_IDX[json["dynamic_follow"].string_value()];
-    s->scene.lsButtonStatus = LS_TO_IDX[json["lane_speed_alerts"].string_value()];
-//    printf("dfButtonStatus: %d\n", s->scene.dfButtonStatus);
-//    printf("lsButtonStatus: %d\n", s->scene.lsButtonStatus);
-  } else {  // error parsing json
-    printf("ERROR PARSING OPPARAMS JSON!\n");
-    s->scene.dfButtonStatus = 0;
-    s->scene.lsButtonStatus = 0;
-  }
+//  // stock additions todo: run opparams first (in main()?) to ensure json values exist
+//  std::ifstream op_params_file("/data/op_params.json");
+//  std::string op_params_content((std::istreambuf_iterator<char>(op_params_file)),
+//                                (std::istreambuf_iterator<char>()));
+//
+//  std::string err;
+//  auto json = json11::Json::parse(op_params_content, err);
+//  if (!json.is_null() && err.empty()) {
+//    printf("successfully parsed opParams json\n");
+//    s->scene.dfButtonStatus = DF_TO_IDX[json["dynamic_follow"].string_value()];
+//    s->scene.lsButtonStatus = LS_TO_IDX[json["lane_speed_alerts"].string_value()];
+////    printf("dfButtonStatus: %d\n", s->scene.dfButtonStatus);
+////    printf("lsButtonStatus: %d\n", s->scene.lsButtonStatus);
+//  } else {  // error parsing json
+//    printf("ERROR PARSING OPPARAMS JSON!\n");
+//    s->scene.dfButtonStatus = 0;
+//    s->scene.lsButtonStatus = 0;
+//  }
   s->scene.mlButtonEnabled = false;  // state isn't saved yet
 }
 
