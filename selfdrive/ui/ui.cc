99f803ed3a0d40a093594ac53bf5863be431a971
Shane Smiskol <shane@smiskol.com>
2021-03-27 00:23:08 -0500

    debug

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -37,7 +37,8 @@ void sa_init(UIState *s, bool full_init) {
 
   char** value;
   size_t sz;
-  *value = static_cast<char*>(read_file("/data/op_params.json", sz));
+  std::string params_path = "/data/community/params/camera_offset";
+  *value = static_cast<char*>(read_file(params_path.c_str(), &sz));
 
 //  // stock additions todo: run opparams first (in main()?) to ensure json values exist
 //  std::ifstream op_params_file("/data/op_params.json");
