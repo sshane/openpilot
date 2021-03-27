7b50b29e9e72f14fa75f78bfc9358a74f1dec45d
Shane Smiskol <shane@smiskol.com>
2021-03-27 00:18:30 -0500

    debug

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -37,7 +37,7 @@ void sa_init(UIState *s, bool full_init) {
 
   char** value;
   size_t sz;
-  *value = static_cast<char*>(read_file("/data/op_params.json".c_str(), sz));
+  *value = static_cast<char*>(read_file("/data/op_params.json", sz));
 
 //  // stock additions todo: run opparams first (in main()?) to ensure json values exist
 //  std::ifstream op_params_file("/data/op_params.json");
