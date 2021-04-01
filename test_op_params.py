c13b0b83b891d768e48aa76f02a18aa05804add9
Shane Smiskol <shane@smiskol.com>
2021-04-01 05:32:15 -0500

    faster

--- test_op_params.py
+++ test_op_params.py
@@ -8,7 +8,7 @@ op = opParams()
 profiles = ['traffic', 'relaxed', 'stock', 'auto']
 
 while 1:
-  time.sleep(random.uniform(.25, 1))
+  time.sleep(random.uniform(.01, .5))
   v = random.choice(profiles)
   op.put('dynamic_follow', v, old=True)
   print('Put dynamic_follow with {}'.format(v), flush=True)
