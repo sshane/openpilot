49c29a3278a2bf1e25ba9c578d6e675fbfae017e
Shane Smiskol <shane@smiskol.com>
2021-04-01 08:48:23 -0500

    bool values don't have quotes

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -44,10 +44,10 @@ void sa_init(UIState *s, bool full_init) {
   }
   std::string model_laneless = util::read_file("/data/community/params/model_laneless");
   if (model_laneless != "") {
-    model_laneless = model_laneless.substr(1, model_laneless.find_last_of('"') - 1);
+    model_laneless = model_laneless.substr(0, model_laneless.find_last_of(' ') - 1);
     std::cout << "model_laneless is  " << model_laneless << std::endl;
     s->scene.end_to_end = model_laneless == "true";
-    std::cout << "end_to_end is  " << model_laneless << std::endl;
+    std::cout << "end_to_end is  " << s->scene.end_to_end << std::endl;
   }
 //  std::string lane_speed_alerts = util::read_file("/data/community/params/lane_speed_alerts");
 //  if (lane_speed_alerts != "") {
