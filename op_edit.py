7f060a9847d7b9a5d036cc0c8c83dd3302083b0c
Shane Smiskol <shane@smiskol.com>
2021-04-01 09:43:57 -0500

    end

--- op_edit.py
+++ op_edit.py
@@ -142,13 +142,13 @@ class opEdit:  # use by running `python /data/openpilot/op_edit.py`
       to_print = []
       if param_info.has_description:
         to_print.append(COLORS.OKGREEN + '>>  Description: {}'.format(param_info.description.replace('\n', '\n  > ')) + COLORS.ENDC)
-      to_print.append(COLORS.OKGREEN + '>>  Default value: {}'.format(self.color_from_type(param_info.default_value)) + COLORS.ENDC)
       if param_info.static:
         to_print.append(COLORS.WARNING + '>>  A reboot is required for changes to this parameter!' + COLORS.ENDC)
       if not param_info.static and not param_info.live:
         to_print.append(COLORS.WARNING + '>>  Changes take effect within 10 seconds for this parameter!' + COLORS.ENDC)
       if param_info.has_allowed_types:
         to_print.append(COLORS.RED + '>>  Allowed types: {}'.format(', '.join([at.__name__ for at in param_info.allowed_types])) + COLORS.ENDC)
+      to_print.append(COLORS.PROMPT + '>>  Default value: {}'.format(self.color_from_type(param_info.default_value)) + COLORS.ENDC)
 
       if to_print:
         print('\n{}\n'.format('\n'.join(to_print)))
@@ -282,7 +282,7 @@ class opEdit:  # use by running `python /data/openpilot/op_edit.py`
     elif style == 'cyan':
       style = COLORS.CYAN
     elif isinstance(style, int):
-      style = COLORS.BASE(86)
+      style = COLORS.BASE(style)
 
     if surround:
       msg = '{}--------\n{}\n{}--------{}'.format(style, msg, COLORS.ENDC + style, COLORS.ENDC)
