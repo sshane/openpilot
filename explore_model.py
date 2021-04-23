0905c8055ddccc0502a4a7f801b191a89b13ad8f
Shane Smiskol <shane@smiskol.com>
2021-04-23 16:13:17 -0500

    print lane lines

---
+++ explore_model.py
@@ -0,0 +1,16 @@
+import cereal.messaging as messaging
+
+
+sm = messaging.SubMaster(['modelV2'])
+
+while 1:
+  sm.update(0)
+
+  print('left lane line:')
+  print(list(sm['modelV2'].modelV2.lanelines[1].y))
+
+  print('right lane line:')
+  print(list(sm['modelV2'].modelV2.lanelines[2].y))
+
+  input()
+
