a1c3ce9fe470cc42b4088953d36905c91d221251
Shane Smiskol <shane@smiskol.com>
2021-04-23 17:01:11 -0500

    fix

--- explore_model.py
+++ explore_model.py
@@ -6,6 +6,9 @@ sm = messaging.SubMaster(['modelV2'])
 while 1:
   sm.update(0)
 
+  if len(sm['modelV2'].laneLines[1]) != 4:
+    continue
+
   print('left lane line:')
   print(list(sm['modelV2'].laneLines[1].y))
 
