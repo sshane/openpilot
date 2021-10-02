00293d839d295242ad2e3c039ef9d9fbe710ad74
sshane <shane@smiskol.com>
2021-10-02 01:52:56 -0500

    tab fixes

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -131,21 +131,21 @@ static void ui_draw_vision_lane_lines(UIState *s) {
     }
   }
 
-	// paint path
-	const cereal::ModelDataV2::XYZTData::Reader &pos = (*s->sm)["modelV2"].getModelV2().getPosition();
-	const float lat_pos = pos.getY().size() > 0 ? std::abs(pos.getY()[14] - pos.getY()[0]) : 0;  // 14 is 1.91406 (subtract initial pos to not consider offset)
-	const float hue = lat_pos * -39.46 + 148;  // interp from {0, 4.5} -> {148, 0}
-	NVGpaint track_bg;
-	if ((*s->sm)["controlsState"].getControlsState().getEnabled()) {
-		track_bg = nvgLinearGradient(s->vg, s->fb_w, s->fb_h, s->fb_w, s->fb_h*.4,
-									nvgHSLA(hue / 360., .94, .51, 255), nvgHSLA(hue / 360., .73, .49, 100));
-	} else if (!scene.end_to_end) {
-		track_bg = nvgLinearGradient(s->vg, s->fb_w, s->fb_h, s->fb_w, s->fb_h * .4,
-                                          COLOR_WHITE, COLOR_WHITE_ALPHA(0));
+  // paint path
+  const cereal::ModelDataV2::XYZTData::Reader &pos = (*s->sm)["modelV2"].getModelV2().getPosition();
+  const float lat_pos = pos.getY().size() > 0 ? std::abs(pos.getY()[14] - pos.getY()[0]) : 0;  // 14 is 1.91406 (subtract initial pos to not consider offset)
+  const float hue = lat_pos * -39.46 + 148;  // interp from {0, 4.5} -> {148, 0}
+  NVGpaint track_bg;
+  if ((*s->sm)["controlsState"].getControlsState().getEnabled()) {
+    track_bg = nvgLinearGradient(s->vg, s->fb_w, s->fb_h, s->fb_w, s->fb_h*.4,
+                                 nvgHSLA(hue / 360., .94, .51, 255), nvgHSLA(hue / 360., .73, .49, 100));
+  } else if (!scene.end_to_end) {
+    track_bg = nvgLinearGradient(s->vg, s->fb_w, s->fb_h, s->fb_w, s->fb_h * .4,
+                                 COLOR_WHITE, COLOR_WHITE_ALPHA(0));
   } else {
     track_bg = nvgLinearGradient(s->vg, s->fb_w, s->fb_h, s->fb_w, s->fb_h * .4,
-                                          COLOR_RED, COLOR_RED_ALPHA(0));
-	}
+                                 COLOR_RED, COLOR_RED_ALPHA(0));
+  }
 
   ui_draw_line(s, scene.track_vertices, nullptr, &track_bg);
 }
