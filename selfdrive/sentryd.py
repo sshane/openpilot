6ed32376646d461aaddc46fee72f36bb8dd9d3ce
Shane Smiskol <shane@smiskol.com>
2022-01-18 03:50:42 -0700

    make the different states more clear

--- selfdrive/sentryd.py
+++ selfdrive/sentryd.py
@@ -10,7 +10,14 @@ from opendbc.can.parser import CANParser
 
 MAX_TIME_ONROAD = 5 * 60.
 MOVEMENT_TIME = 1. * 60  # normal time allowed is one minute
-OFFROAD_TIME = 1. * 30  # needs to be offroad for this time before sentry mode is active
+INTERACTION_TIME = 1. * 30  # car needs to be inactive for this time before sentry mode is enabled
+
+
+# Sentry mode state legend:
+# Enabled: parameter is set allowing operation
+# Armed: watching for car movement
+# Tripped: movement tripped sentry mode, recording and alarming
+# Car active: any action that signifies a user is present and interactiving with their car
 
 
 class SentryMode:
@@ -18,36 +25,27 @@ class SentryMode:
     self.sm = messaging.SubMaster(['deviceState', 'sensorEvents'], poll=['sensorEvents'])
     self.pm = messaging.PubMaster(['sentryState'])
 
-    signals = [
-      ("DOOR_LOCK_FEEDBACK_LIGHT", "CENTRAL_GATEWAY_UNIT", 0),
-      ("KEYFOB_LOCKING_FEEDBACK_LIGHT", "CENTRAL_GATEWAY_UNIT", 0),
-      ("KEYFOB_UNLOCKING_FEEDBACK_LIGHT", "CENTRAL_GATEWAY_UNIT", 0),
-    ]
-    self.cp = CANParser("toyota_nodsu_pt_generated", signals, bus=0, enforce_checks=False)
+    self.cp = CANParser("toyota_nodsu_pt_generated", [("DOOR_LOCK_FEEDBACK_LIGHT", "CENTRAL_GATEWAY_UNIT", 0)], bus=0, enforce_checks=False)
     self.can_sock = messaging.sub_sock('can', timeout=100)
+
+    self.prev_accel = np.zeros(3)
+    self.initialized = False
 
     self.params = Params()
     self.sentry_enabled = self.params.get_bool("SentryMode")
     self.last_read_ts = sec_since_boot()
 
-    self.prev_accel = np.zeros(3)
-    self.initialized = False
-    self.started = False
-    self.prev_started = False
-    self.started_ts = 0.
+    self.sentry_tripped = False
+    self.prev_sentry_tripped = False
+    self.sentry_tripped_ts = 0.
+    self.car_active_ts = sec_since_boot()  # start at active
     self.movement_ts = 0.
     self.accel_filters = [FirstOrderFilter(0, 0.5, DT_CTRL) for _ in range(3)]
 
   def update(self):
+    # Update CAN
     can_strs = messaging.drain_sock_raw(self.can_sock, wait_for_one=True)
     self.cp.update_strings(can_strs)
-
-    print('DOOR_LOCK_FEEDBACK_LIGHT: {}'.format(bool(self.cp.vl["CENTRAL_GATEWAY_UNIT"]["DOOR_LOCK_FEEDBACK_LIGHT"])))
-    print('KEYFOB_LOCKING_FEEDBACK_LIGHT: {}'.format(bool(self.cp.vl["CENTRAL_GATEWAY_UNIT"]["KEYFOB_LOCKING_FEEDBACK_LIGHT"])))
-    print('KEYFOB_UNLOCKING_FEEDBACK_LIGHT: {}\n---'.format(bool(self.cp.vl["CENTRAL_GATEWAY_UNIT"]["KEYFOB_UNLOCKING_FEEDBACK_LIGHT"])))
-
-
-    return
 
     # Update parameter
     now_ts = sec_since_boot()
@@ -55,52 +53,62 @@ class SentryMode:
       self.sentry_enabled = self.params.get_bool("SentryMode")
       self.last_read_ts = float(now_ts)
 
+    # Handle sensors
     for sensor in self.sm['sensorEvents']:
       if sensor.which() == 'acceleration':
         accels = sensor.acceleration.v
         if len(accels) == 3:  # sometimes is empty, in that case don't update
-          if self.initialized:
+          if self.initialized:  # prevent initial jump # TODO: can remove since we start at an active car state?
             for idx, v in enumerate(accels):
               self.accel_filters[idx].update(accels[idx] - self.prev_accel[idx])
           self.initialized = True
           self.prev_accel = list(accels)
 
-    self.started = True  # self.get_started(now_ts)
-    print(f"{self.started=}")
+    self.sentry_tripped = self.is_sentry_tripped(now_ts)
+    print(f"{self.sentry_tripped=}")
 
-    if self.started and not self.prev_started:
-      self.started_ts = sec_since_boot()
+    if self.sentry_tripped and not self.prev_sentry_tripped:
+      self.sentry_tripped_ts = sec_since_boot()
 
-    self.prev_started = self.started
+    self.prev_sentry_tripped = self.sentry_tripped
 
-  def get_started(self, now_ts):
-    offroad = not self.sm['deviceState'].started
-    offroad_long_enough = now_ts - (self.sm['deviceState'].offMonoTime / 1e9) > OFFROAD_TIME  # needs to be offroad for 30 sec
+  def is_sentry_armed(self, now_ts):
+    """Returns if sentry is actively monitoring for movements/can be alarmed"""
+    # Handle car interaction, reset interaction timeout
+    car_active = self.sm['deviceState'].started
+    car_active = car_active or self.cp.vl["CENTRAL_GATEWAY_UNIT"]["DOOR_LOCK_FEEDBACK_LIGHT"]
+    if car_active:
+      self.car_active_ts = float(now_ts)
 
+    car_inactive_long_enough = now_ts - self.car_active_ts > INTERACTION_TIME  # needs to be inactive for long enough
+    return car_inactive_long_enough
+
+  def is_sentry_tripped(self, now_ts):
     movement = any([abs(a_filter.x) > .01 for a_filter in self.accel_filters])
     if movement:
       self.movement_ts = float(now_ts)
-    # print([a_filter.x for a_filter in self.accel_filters])
 
     # Maximum allowed time onroad without movement is 1 minute. Any movement resets time allowed, maximum time is 5 minutes
     # TODO: can remove started_ts if time is the same
-    onroad_long_enough = (now_ts - self.started_ts > MOVEMENT_TIME and now_ts - self.movement_ts > MOVEMENT_TIME) or now_ts - self.started_ts > MAX_TIME_ONROAD
+    tripped_long_enough = (now_ts - self.sentry_tripped_ts > MOVEMENT_TIME and now_ts - self.movement_ts > MOVEMENT_TIME)
+    tripped_long_enough = tripped_long_enough or now_ts - self.sentry_tripped_ts > MAX_TIME_ONROAD  # or total time reached
 
-    started = False
-    print(f"{offroad=}, {offroad_long_enough=}, {movement=}")
-    print(f"{onroad_long_enough=}")
-    print(f"{now_ts - self.started_ts=}")
-    if offroad and self.sentry_enabled:  # car's ignitions needs to be off (not started by user)
-      if offroad_long_enough and movement:
-        started = True
-      elif self.started and not onroad_long_enough:
-        started = True
+    sentry_tripped = False
+    sentry_armed = self.is_sentry_armed()
+    print(f"{sentry_armed=}, {sentry_armed=}, {movement=}")
+    print(f"{tripped_long_enough=}")
+    print(f"{now_ts - self.sentry_tripped_ts=}")
+    if sentry_armed and self.sentry_enabled:
+      if movement:  # trip if armed, enabled, and there's movement
+        sentry_tripped = True
+      elif self.sentry_tripped and not tripped_long_enough:  # trip for long enough
+        sentry_tripped = True
 
-    return started
+    return sentry_tripped
 
   def publish(self):
     sentry_state = messaging.new_message('sentryState')
-    sentry_state.sentryState.started = self.started
+    sentry_state.sentryState.started = self.sentry_tripped
 
     self.pm.send('sentryState', sentry_state)
 
