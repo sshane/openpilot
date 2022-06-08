4682b53cdffa4fc67dcef4aee3ffa66476fa84e6
AtLmd <44336271+AtLmd@users.noreply.github.com>
2022-06-08 20:45:10 +0200

    AtL's INDI Corolla TSS2 Tune v2.0
    I updated the INDI tune of Corollas TSS2 on OP0813 as birdman's tune was a bit off not being maintained for so long.
    Maybe it would be good too for OP0814 but I can't test it as I only have a C2.
    Hope it's not too late and that C2 branch is not staled.

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -158,17 +158,17 @@ class CarInterface(CarInterfaceBase):
       tire_stiffness_factor = 0.444  # not optimized yet
       ret.mass = 3060. * CV.LB_TO_KG + STD_CARGO_KG
 
-      if lat_params.corollaTSS2_use_indi:  # birdman6450#7399's Corolla 2020 TSS2 Tune
+      if lat_params.corollaTSS2_use_indi:  # AtL's INDI Corolla TSS2 Tune v2.0
         ret.lateralTuning.init('indi')
-        ret.lateralTuning.indi.innerLoopGainBP = [18, 22, 26]
-        ret.lateralTuning.indi.innerLoopGainV = [9, 12, 15]
-        ret.lateralTuning.indi.outerLoopGainBP = [18, 22, 26]
-        ret.lateralTuning.indi.outerLoopGainV = [8, 11, 14.99]
-        ret.lateralTuning.indi.timeConstantBP = [18, 22, 26]
-        ret.lateralTuning.indi.timeConstantV = [1, 3, 4.5]
-        ret.lateralTuning.indi.actuatorEffectivenessBP = [18, 22, 26]
-        ret.lateralTuning.indi.actuatorEffectivenessV = [9, 12, 15]
-        ret.steerActuatorDelay = 0.42 - 0.2
+        ret.lateralTuning.indi.innerLoopGainBP = [0.]
+        ret.lateralTuning.indi.innerLoopGainV = [15]
+        ret.lateralTuning.indi.outerLoopGainBP = [0.]
+        ret.lateralTuning.indi.outerLoopGainV = [17]
+        ret.lateralTuning.indi.timeConstantBP = [0.]
+        ret.lateralTuning.indi.timeConstantV = [4.5]
+        ret.lateralTuning.indi.actuatorEffectivenessBP = [0.]
+        ret.lateralTuning.indi.actuatorEffectivenessV = [15]
+        ret.steerActuatorDelay = 0.41
       else:
         set_lat_tune(ret.lateralTuning, lat_params, LatTunes.PID_D)
 
