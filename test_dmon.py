1802c7508ccadf5afb096e76e9f8dfb41f4543ee
Shane Smiskol <shane@smiskol.com>
2023-07-18 21:20:46 -0700

    prints, wait 5s

--- test_dmon.py
+++ test_dmon.py
@@ -13,16 +13,17 @@ if __name__ == "__main__":
   while 1:
     params.put_bool("FakeIgnition", True)
     sm.update(0)
+    dmon_frame = sm.rcv_frame['driverStateV2']
 
     print('Waiting for driverStateV2')
-    dmon_frame = sm.rcv_frame['driverStateV2']
     st = time.monotonic()
     timeout = 15  # s
-    while sm.rcv_frame['driverStateV2'] == dmon_frame and time.monotonic() - st < timeout:
+    while (sm.rcv_frame['driverStateV2'] - dmon_frame) > 5 * 20 and time.monotonic() - st < timeout:
       sm.update(0)
-      time.sleep(0.01)
+      time.sleep(0.05)
+
     if sm.rcv_frame['driverStateV2'] == dmon_frame:
-      print('WARNING: never saw frame from driverStateV2 in 15 seconds')
+      print('WARNING: never saw frame from driverStateV2 in 15 seconds', sm.rcv_frame['driverStateV2'], dmon_frame)
     else:
       print('Got driverStateV2! Exiting', sm.rcv_frame['driverStateV2'], dmon_frame)
       time.sleep(1)
