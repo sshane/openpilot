17aff44c6f52667515d17e28957a050e1a5b2df7
Shane Smiskol <shane@smiskol.com>
2023-07-18 21:33:36 -0700

    wait for death

--- test_dmon.py
+++ test_dmon.py
@@ -3,12 +3,16 @@ from cereal.messaging import SubMaster
 from common.params import Params
 
 
+def all_dead(managerState):
+  # return if all processes that should be dead are dead
+  return all([not p.running or p.shouldBeRunning for p in managerState.processes])
+
+
 if __name__ == "__main__":
   params = Params()
   params.put_bool("FakeIgnition", False)
 
-  sm = SubMaster(["driverStateV2"])
-
+  sm = SubMaster(["driverStateV2", "managerState"])
 
   while 1:
     params.put_bool("FakeIgnition", True)
@@ -18,7 +22,7 @@ if __name__ == "__main__":
     print('Waiting for driverStateV2')
     st = time.monotonic()
     timeout = 15  # s
-    while (sm.rcv_frame['driverStateV2'] - dmon_frame) < 5 * 20 and time.monotonic() - st < timeout:
+    while (sm.rcv_frame['driverStateV2'] - dmon_frame) < (5 * 20) and (time.monotonic() - st < timeout):
       sm.update(0)
       time.sleep(0.05)
 
@@ -29,5 +33,11 @@ if __name__ == "__main__":
       time.sleep(1)
 
     params.put_bool("FakeIgnition", False)
-    time.sleep(5)
-
+    st = time.monotonic()
+    while not all_dead(sm['managerState']) and (time.monotonic() - st < 5):
+      sm.update(0)
+    if not all_dead(sm['managerState']):
+      print('WARNING: timed out waiting for processes to die')
+      time.sleep(5)
+    else:
+      print('all dead')
