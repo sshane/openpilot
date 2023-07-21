7fe700c8e676bd0f579dba1aefebc6b52e0f4f25
Shane Smiskol <shane@smiskol.com>
2023-07-20 18:51:32 -0700

    random delay and order

--- repro_dmon.py
+++ repro_dmon.py
@@ -1,8 +1,10 @@
+import random
 import time
 from selfdrive.manager.process_config import managed_processes
 from common.params import Params
 
 INIT_PARAMS = ["DmModelInit", "NavModelInit"]
+PROCS = ["dmonitoringmodeld", "navmodeld"]
 
 
 def get_init_params(_params):
@@ -13,24 +15,30 @@ if __name__ == "__main__":
   params = Params()
 
   while 1:
-    print('Starting')
+    print('\nStarting')
     for p in INIT_PARAMS:
       params.put_bool(p, False)
 
     st = time.monotonic()
-    managed_processes["dmonitoringmodeld"].start()
-    managed_processes["navmodeld"].start()
+    procs = PROCS.copy()
+    random.shuffle(procs)
+    for proc in procs:
+      managed_processes[proc].start()
+      time.sleep(random.uniform(0, 250) / 1000)  # 0 to 250 ms offset
 
     while time.monotonic() - st < 10:
+      print('not alive')
       time.sleep(0.1)
       if all(get_init_params(params)):
+        print('both done init')
         break
     else:
       pd = dict(zip(INIT_PARAMS, get_init_params(params)))
       print('WARNING: timed out waiting for model procs to initialize!', pd)
 
-    managed_processes["dmonitoringmodeld"].stop()
-    managed_processes["navmodeld"].stop()
+    for proc in procs:
+      print('Stopping', proc)
+      managed_processes[proc].stop()
 
 
 
