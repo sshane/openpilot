02df2668e0b16f96f3e0de9a750b4e19f11c4588
sshane <shane@smiskol.com>
2023-09-30 03:37:11 -0700

    decode

---
+++ bad/decode_frames.py
@@ -0,0 +1,44 @@
+# from tools.lib.framereader import FrameReader
+from tools.lib.logreader import LogReader
+
+import cv2
+
+# FrameReader()
+FRAME_SIZE = (1928, 1208)
+
+TEST_SEG = '/mnt/c/Users/Shane/Downloads/body-data/c81675c456f9f72d_2023-09-27--17-23-04/5'
+# TEST_SEG = 'C:/Users/Shane/Downloads/body-data/c81675c456f9f72d_2023-09-27--17-23-04/5/dcamera.hevc'
+vidcap = cv2.VideoCapture(f'{TEST_SEG}/dcamera.hevc')
+lr = LogReader(f'{TEST_SEG}/rlog.bz2')
+
+tjs = 0
+train_y = []
+for msg in sorted(lr, key=lambda x: x.logMonoTime):
+  if msg.which() == 'testJoystick':
+    if len(msg.testJoystick.axes):
+      print(msg.testJoystick.axes)
+      train_y.append(msg.testJoystick.axes)  # left/right is idx 1, fwd/bck is idx 0
+      tjs += 1
+# raise Exception
+print(tjs)
+
+# forward is -1, backward is 1
+# left is 1, right is -1
+count = 0
+success = True
+every = True
+while success:
+  success, image = vidcap.read()
+  image = cv2.resize(image, (round(FRAME_SIZE[0] / 3), round(FRAME_SIZE[1] / 3)))
+  if count % 20 == 0:
+    train = 'train' if every else 'test'
+    every = not every
+    img_fn = "/mnt/c/Users/Shane/bad/{}/image{:>3}_y{}_{}.png".format(train, count,
+                                                                      round(train_y[count + 10][0]),
+                                                                      round(train_y[count + 10][1]))
+    cv2.imwrite(img_fn, image)
+    print(img_fn)
+  print(image.shape)
+  # break
+  print('Read a new frame: ', success, count)
+  count += 1
--- bad/train.py
+++ bad/train.py
@@ -1,19 +1,34 @@
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
+import os
+import cv2
 
-SAMPLES = 100
-X = np.random.random(((100, 1920, 1080, 3)))
+IMGS_PATH = 'C:/Users/Shane/bad'
+
+images = []
+print(os.listdir(IMGS_PATH))
+for img_fn in os.listdir(IMGS_PATH):
+  images.append(cv2.imread(f'{IMGS_PATH}/{img_fn}'))
+
+# raise Exception
+SAMPLES = len(images)
+# RESOLUTION = images[0].shape[:2]  # (960, 540)
+# print(f'Resolution: {RESOLUTION}')
+# raise Exception
+# X = np.random.random(((SAMPLES, 1920, 1080, 3)))
+X = np.array(images)
 Y = np.random.random((SAMPLES,))
 
-INPUT_SHAPE=(1920, 1080, 3)
-
+INPUT_SHAPE = images[0].shape
+print(f'X shape: {X.shape}')
+print(f'Input shape: {INPUT_SHAPE}')
 
 model = tf.keras.Sequential([
-  tf.keras.layers.Conv2D(2, 3, activation='relu', input_shape=INPUT_SHAPE)
+  tf.keras.layers.Conv2D(3, 3, activation='relu', input_shape=INPUT_SHAPE),
+  tf.keras.layers.Dense(1, activation='relu'),
 ])
 
 model.compile(optimizer='adam', loss='mse')
 
-model.fit(X, Y, batch_size=32, epochs=2)
-
+model.fit(X, Y, batch_size=2, epochs=10)
