206098a35cb47120b8515ba456e329c9ab92b838
Shane Smiskol <shane@smiskol.com>
2024-02-14 03:56:44 -0800

    meh

--- tools/lib/route.py
+++ tools/lib/route.py
@@ -273,16 +273,18 @@ class SegmentRange:
     assert m is not None, f"Invalid slice: {self._slice}"
     start, end, step = (None if s is None else int(s) for s in m.groups())
 
+    # positive start is easier to work with
+    if start is not None and start < 0:
+      start += get_max_seg_number_cached(self) + 1
+
     # if start is specified without end, set end to get a single segment
     if start is not None and end is None and ':' not in self._slice:
-      if start < 0:
-        end = start + get_max_seg_number_cached(self) + 2
-      else:
-        end = start + 1
+      end = start + 1
 
-    relative = end is None or end < 0 or (start is not None and start < 0)
-    max_seg = (get_max_seg_number_cached(self) if relative else end) + 1
-    return list(range(max_seg))[slice(start, end, step)]
+    if end is None or end < 0 or (start is not None and start < 0):
+      return list(range(get_max_seg_number_cached(self) + 1))[slice(start, end, step)]
+    else:
+      return list(range(end + 1))[slice(start, end, step)]
 
   @property
   def selector(self) -> str:
